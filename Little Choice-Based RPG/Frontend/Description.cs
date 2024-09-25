2edbddf7edd49d3aec0d0f0a1303b07015cfe79b
ReeceBDev <reece.bowen99@gmail.com>
2024-09-26 00:06:30 +0100

    Added player descriptor and get room descriptor functionality.

--- Little Choice-Based RPG/Frontend/Description.cs
+++ Little Choice-Based RPG/Frontend/Description.cs
@@ -1,5 +1,7 @@
 ﻿using Little_Choice_Based_RPG.Entities.Derived.Living.Players;
 using Little_Choice_Based_RPG.Types;
+using Little_Choice_Based_RPG.World.Managers;
+using Little_Choice_Based_RPG.World.Rooms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +10,23 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Frontend
 {
-    internal class Description
+    public class Description
     {
         private protected SanitizedString contextualDescription;
         private protected SanitizedString baseDescription;
 
         public string GetDescription(Player player)
         {
+            return player.PlayerDescriptor.Value;
+        }
+        public string GetRoomDescriptor(GameEnvironment currentEnvironment, uint roomToDescribe)
+        {
+            Room foundRoom = currentEnvironment.FindRoomByID(roomToDescribe);
 
+            if (foundRoom == null)
+                throw new ArgumentNullException("Room not found in given environment.");
+
+            return foundRoom.Descriptors.initial;
         }
     }
 }
