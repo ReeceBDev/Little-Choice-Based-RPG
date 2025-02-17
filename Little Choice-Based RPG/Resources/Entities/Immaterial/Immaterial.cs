76fee48f51f153358a65ab5b21442dbbaa266da5
ReeceBDev <reece.bowen99@gmail.com>
2025-02-17 22:21:32 +0000

    Added immaterial object class to inherit from.

---
+++ Little Choice-Based RPG/Resources/Entities/Immaterial/Immaterial.cs
@@ -0,0 +1,17 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.Immaterial
+{
+    public class Immaterial : GameObject
+    {
+        public Immaterial(string setName) : base(setName)
+        {
+            entityProperties.Add(new EntityProperty("isImmaterial", true));
+        }
+    }
+}
