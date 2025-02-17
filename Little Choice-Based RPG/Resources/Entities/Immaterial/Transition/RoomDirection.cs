eeb8923728e4c8c0feada044de073ab068e6c037
ReeceBDev <reece.bowen99@gmail.com>
2025-02-17 22:21:21 +0000

    Moved RoomDirection to its own class and file. Rebuilt RoomDirection to conform to new requirements. Added new variables and added default functionality. Hide DestinationID behind a check which confirms that an object associated with the direction is the only thing able to unlock it, i.e. a door can use a direction built only for itself. Added descriptor. Made immaterial.

---
+++ Little Choice-Based RPG/Resources/Entities/Immaterial/Transition/RoomDirection.cs
@@ -0,0 +1,41 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.Immaterial.Transition
+{
+    public class RoomDirection : Immaterial
+    {
+        private string defaultDescriptor = $"Feet scraping flecks of shrubbery drowned in dust scrambling from cracked sandstone, you forge your way further across the desolation.";
+        public RoomDirection(string setDirectionName, uint setDestinationRoomID, string setTravelDescriptor = "", uint setDependentObjectID = 0) : base(setDirectionName)
+        {
+            DestinationRoomID = setDestinationRoomID;
+            TravelDescriptor = (setTravelDescriptor == "") ? defaultDescriptor : setTravelDescriptor;
+
+            if (setDependentObjectID > 0)
+            {
+                DirectionIsVisible = false;
+                DependentObjectID = setDependentObjectID;
+            }
+
+            entityProperties.Add(new EntityProperty("isImmaterial", true));
+        }
+
+        public uint GetDestinationID(GameObject? usingKey = null) // Returns 0 if the key does not match DependentObjectID. usingKey should be the calling object.
+        {
+            uint keyID = (usingKey != null) ? usingKey.ID : 0;
+
+            return (keyID == DependentObjectID) ? DestinationRoomID : 0;
+        }
+
+        
+        public string TravelDescriptor { get; private set; }
+        public bool DirectionIsVisible { get; private set; } = true;
+        public uint DependentObjectID { get; private set; } = 0; //0 must be Invalid because there is no objectID that can be 0.
+        private uint DestinationRoomID { get; set; }
+    }
+}
