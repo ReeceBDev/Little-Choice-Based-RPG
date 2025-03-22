18b3f5c75268b08daa569a0d4115e991bacadd51
ReeceBDev <reece.bowen99@gmail.com>
2025-03-22 23:56:20 +0000

    Outlined new EntityProperty class to replace the existing EntityProperty.

---
+++ Little Choice-Based RPG/Types/EntityProperty/EntityProperty.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.EntityProperty 
+{ 
+
+public struct EntityProperty
+    {
+        private string propertyName;
+        private Object propertyValue;
+        private bool isReadOnly;
+
+        public EntityProperty(string setPropertyName, Object setPropertyValue)
+        {
+            isReadOnly = false;
+
+            propertyName = setPropertyName;
+
+            propertyValue = setPropertyValue;
+        }
+
+        public EntityProperty(string setPropertyName, Object setPropertyValue, bool setIsFrozen)
+        {
+            isReadOnly = setIsFrozen;
+
+            propertyName = setPropertyName;
+
+            attemptedPropertyValueType = setPropertyValue.GetType()
+
+            if ( == bool)
+            {
+                propertyValue = setPropertyValue;
+            }
+
+        }
+        public string PropertyName { get; private set; }
+
+        public Object PropertyValue { get; private set; }
+        public bool ReadOnly { get; private set; }
+    }
+}
