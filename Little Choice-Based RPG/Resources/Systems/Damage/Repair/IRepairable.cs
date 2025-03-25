2d8c494f0dd02f9235e56eed8085af0dcaf6ce71
ReeceBDev <reece.bowen99@gmail.com>
2025-03-25 14:45:09 +0000

    Created RepairSystem using IRepairable interface. Moved related existing PropertyValidation validProperties into a RepairSystem static constructor. Added additional validProperties.

---
+++ Little Choice-Based RPG/Resources/Systems/Damage/Repair/IRepairable.cs
@@ -0,0 +1,14 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
+{
+    public interface IRepairable
+    {
+        void Repair(GameObject withRepairAsset);
+    }
+}
---
+++ Little Choice-Based RPG/Resources/Systems/Damage/Repair/RepairSystem.cs
@@ -0,0 +1,30 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Systems.Damage.Repair;
+using Little_Choice_Based_RPG.Types.EntityProperty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
+{
+    internal class RepairSystem : IRepairable
+    {
+        static RepairSystem()
+        {
+            //RepairSystem logic
+            PropertyValidation.CreateValidProperty("IsRepairable", PropertyType.Boolean); //Activates all this class and all of these properties when true :)
+            PropertyValidation.CreateValidProperty("IsRepairableByChoice", PropertyType.Boolean); //Lets players choose to repair it by choice.
+
+            //Descriptors
+            PropertyValidation.CreateValidProperty("Descriptor.Repairing", PropertyType.String); //Describes how it looks when it gets repaired.
+            PropertyValidation.CreateValidProperty("Descriptor.Choice.Repair", PropertyType.String); //Describes the action of repairing it when a player uses the Repair() choice.
+        }
+
+        public void Repair(GameObject withRepairAsset)
+        {
+
+        }
+    }
+}
