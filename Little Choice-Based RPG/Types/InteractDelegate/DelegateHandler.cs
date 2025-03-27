6cc199c6ab0009fb371aaf89c1caadd10ca842bf
ReeceBDev <reece.bowen99@gmail.com>
2025-03-27 20:30:28 +0000

    Added new class DelegateHandler which aims to retrieve data sources outlined in delegateValidation

---
+++ Little Choice-Based RPG/Types/InteractDelegate/DelegateHandler.cs
@@ -0,0 +1,43 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.InteractDelegate
+{
+    internal class DelegateHandler ///I want this class to be responsible for finding and putting together information sources outlined in DelegateValidation.
+    {
+
+        private static Dictionary<DelegateParameter, Delegate> parameterHandlers = new Dictionary<DelegateParameter, Delegate>()
+        {
+            { DelegateParameter.Source_GameObject, handleSource_GameObjectDelegate }
+        };
+
+        static DelegateHandler() //Validate that each DelegateParameter is handled by a method here, and create a switch statement in GetParameter with them all in.
+        {
+            Enum[] validDelegateParameters = GetValues(DelegateParameter));
+
+            foreach (var enumName in Enum.GetValues(typeof(DelegateParameter)))
+            {
+                Delegate.CreateDelegate(typeof(object), this, "handleSource_GameObjectDelegate", handleSource_GameObject);
+            }
+        }
+        
+        public static T GetParameter<T>(DelegateParameter requiredParameter) // Entry point. Should basically be a dynamically written switch statement pointing to delegates of other methods.
+        {
+            parameterHandlers.TryGetValue(requiredParameter).Invoke();
+        }
+
+        private static object handleSource_GameObject()
+        {   
+
+        }
+
+
+    }
+}
