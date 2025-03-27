fd3d5c8bdb78a1b37aa3fe60338bbef964b265d8
ReeceBDev <reece.bowen99@gmail.com>
2025-03-27 20:31:47 +0000

    Added new class InteractDelegates to declare the delegates which will be used to link methods to player actions. This is a work in progress.

---
+++ Little Choice-Based RPG/Types/InteractDelegate/InteractDelegates.cs
@@ -0,0 +1,27 @@
+﻿using Little_Choice_Based_RPG.Resources.Choices;
+using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.InteractDelegate
+{
+    public static class InteractDelegates //Declares All Interact Delegates
+    {
+        public delegate void InteractOnSourceProperties(PropertyHandler Source_PropertyHandler);
+        public delegate void InteractOnSourcePropertiesUsingTargetObject(PropertyHandler Source_PropertyHandler, GameObject Target_GameObject);
+
+        static InteractDelegates()
+        {
+            // Use reflection to make these. I can then validate them, too, to see if they match perfectly the DelegateParameters.
+            // Doing it this way around lets the delegates also be strongly typed and already seen by VS :)
+            //
+            // I CAN ALSO then use reflection to check if all the delegateValidations are handled by ChoiceHandler :)
+            DelegateValidation.CreateValidDelegate("InteractOnSourceProperties", [DelegateParameter.Source_PropertyHandler]);
+            DelegateValidation.CreateValidDelegate("InteractOnSourcePropertiesUsingTargetObject", [DelegateParameter.Source_PropertyHandler, DelegateParameter.Target_GameObject]);
+        }
+    }
+}
