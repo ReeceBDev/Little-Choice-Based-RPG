cbf29bcb95a378854582ceac5d8cc48e241e3013
ReeceBDev <reece.bowen99@gmail.com>
2025-03-28 04:14:12 +0000

    Created new Singleton named DamageCommon for initialising dependencies between multiple damage systems. Put IsBroken into here from BreakSystem.

---
+++ Little Choice-Based RPG/Resources/Systems/Damage/DamageCommon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Little_Choice_Based_RPG.Types.EntityProperty;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Damage
+{
+    public sealed class DamageCommon
+    {
+        private static readonly DamageCommon singletonInstance = new DamageCommon();
+        private DamageCommon()
+        {
+            PropertyValidation.CreateValidProperty("IsBroken", PropertyType.Boolean); //Indicates when an object becomes broken. Used by BreakSystem and RepairSystem.
+        }
+
+        public static DamageCommon Instance
+        { 
+            get
+            {
+                return singletonInstance;
+            }
+        }
+    }
+}
