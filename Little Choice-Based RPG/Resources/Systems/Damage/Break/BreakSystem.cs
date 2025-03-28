b5982595b4a51cf19efa178c3541eac9931cc783
ReeceBDev <reece.bowen99@gmail.com>
2025-03-28 04:18:14 +0000

    Added constructor for enforcing IRepairable on instantiating object. Instiates new damageCommon class. Made public. Added local storage for instantiating object. Added a few new properties. Moved one property to DamageCommon (IsBroken).  Adds summaries. Added main breaking logic.

--- Little Choice-Based RPG/Resources/Systems/Damage/Break/BreakSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/Damage/Break/BreakSystem.cs
@@ -1,4 +1,6 @@
-﻿using Little_Choice_Based_RPG.Types.EntityProperty;
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Systems.Damage.Repair;
+using Little_Choice_Based_RPG.Types.EntityProperty;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,35 +10,61 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Break
 {
-    internal class BreakSystem : IBreakable
+    public class BreakSystem : IBreakable
     {
+        private GameObject parentObject;
+
         static BreakSystem()
         {
             //BreakSystem logic
             PropertyValidation.CreateValidProperty("IsBreakable", PropertyType.Boolean); //Activates all this class and all of these properties when true :)
-            PropertyValidation.CreateValidProperty("IsBreakableByChoice", PropertyType.Boolean); //Lets players choose to break it by choice.
-            PropertyValidation.CreateValidProperty("IsBroken", PropertyType.Boolean); 
+            PropertyValidation.CreateValidProperty("IsBreakableByChoice", PropertyType.Boolean); //Lets players choose to break it by choice. 
 
             //Descriptors
-            PropertyValidation.CreateValidProperty("Descriptor.Breaking", PropertyType.String); //Describes how it looks when it breaks.
+            PropertyValidation.CreateValidProperty("Descriptor.Breaking", PropertyType.String); //Describes how it looks when it is breaking.
+            PropertyValidation.CreateValidProperty("Descriptor.Choice.Break.Interact", PropertyType.String); //Describes the interact option presented to the player.
+            PropertyValidation.CreateValidProperty("Descriptor.Choice.Breaking", PropertyType.String); //Describes the action of breaking it when a player uses the Break() choice.
             PropertyValidation.CreateValidProperty("Descriptor.Generic.Broken", PropertyType.String); //Broken at a distance
             PropertyValidation.CreateValidProperty("Descriptor.Inspect.Broken", PropertyType.String); //A closer look
-            PropertyValidation.CreateValidProperty("Descriptor.Choice.Break", PropertyType.String); //Describes the action of breaking it when a player uses the Break() choice.
         }
 
-        public void Break(PropertyHandler sourcePropertyHandler)
+        /// <summary> Allows objects to break. Requires DamageCommon. </summary>
+        public BreakSystem(GameObject instantiatingObject)
         {
-            if (!sourcePropertyHandler.HasPropertyAndValue("IsBreakable", true))
+            //Enforces IRepairable on the instantiating class
+            if (!(instantiatingObject is IBreakable))
+                throw new Exception($"{instantiatingObject.GetType()} does not implement IBreakable!");
+
+            DamageCommon damageCommonInstantisation = DamageCommon.Instance;
+
+            //Stores instantiating object for accessing its PropertyHandler.
+            parentObject = instantiatingObject;
+        }
+
+        /// <summary> Sets IsBroken to true. </summary>
+        public void Break()
+        {
+            //Guard clauses for the values in use.
+            if (!parentObject.entityProperties.HasPropertyAndValue("IsBreakable", true))
                 throw new Exception("This object is not breakable! Tried to break an object where there is no EntityProperty of IsBreakable = true.");
 
-            if (!sourcePropertyHandler.HasProperty("Descriptor.Breaking"))
+            if (!parentObject.entityProperties.HasProperty("Descriptor.Breaking"))
                 throw new Exception("This object has no break description! Tried to break an object where there is no EntityProperty of Descriptor.Breaking.");
 
-            if (!sourcePropertyHandler.HasProperty("Descriptor.Generic.Broken") | !sourcePropertyHandler.HasProperty("Descriptor.Inspect.Broken"))
-                throw new Exception("This object has no broken description! Tried to break an object where there is no EntityProperty of Descriptor.Generic.Broken or Descriptor.Generic.Inspect.");
+            if (!parentObject.entityProperties.HasProperty("Descriptor.Generic.Broken"))
+                throw new Exception("This object has no broken description! Tried to break an object where there is no EntityProperty of Descriptor.Generic.Broken.");
 
-            sourcePropertyHandler.UpsertProperty("IsBroken", true);
+            //Main breaking logic.
+            parentObject.entityProperties.UpsertProperty("IsBroken", true); //Property found in DamageCommon
 
+            //Set generic descriptor.
+            parentObject.entityProperties.UpdateProperty("Descriptor.Generic.Current", parentObject.entityProperties.GetPropertyValue("Descriptor.Generic.Broken"));
+
+            //Set inspect descriptor or default to generic.
+            if (!parentObject.entityProperties.HasProperty("Descriptor.Inspect.Broken"))
+                parentObject.entityProperties.UpdateProperty("Descriptor.Inspect.Current", parentObject.entityProperties.GetPropertyValue("Descriptor.Inspect.Broken"));
+            else
+                parentObject.entityProperties.UpdateProperty("Descriptor.Inspect.Current", parentObject.entityProperties.GetPropertyValue("Descriptor.Generic.Broken"));
         }
     }
 }
