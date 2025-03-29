afcf70c59e42cb088db77e7b0747d485243a7e00
ReeceBDev <reece.bowen99@gmail.com>
2025-03-29 00:08:19 +0000

    Created new enum InteractionContext to later replace ChoiceRole.

---
+++ Little Choice-Based RPG/Types/InteractDelegate/InteractionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.InteractDelegate
+{
+    /// <summary> Describes how an Interaction should be presented by the User Interface, for example, if it belongs to a context-menu. </summary>
+    public enum InteractionContext
+    {
+        Explore, //Default. Should show most of the time, such as when exploring or when opening an inspect context-menu.
+        Inspect, //Should be applied upon inspecting an object and opening its context-menu.
+        Navigation, //Should be applied to choices relating to moving around, such as Direction.
+        System //Should apply to anything relating to the meta system, for example, exiting the game, navigating to the options menu, and so on.
+    }
+
+}
