2165fe839ab19ebe2338be8a886708fe9adab270
ReeceBDev <reece.bowen99@gmail.com>
2025-03-29 00:04:40 +0000

    Removed PropertyType from this file and moved into its own file.

--- Little Choice-Based RPG/Types/EntityProperty/PropertyValidation.cs
+++ Little Choice-Based RPG/Types/EntityProperty/PropertyValidation.cs
@@ -7,14 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Little_Choice_Based_RPG.Types.EntityProperty
 {
-    /// <summary> Defines all possible types that may be contained within a property. Properties only contain one PropertyType each. </summary>
-    enum PropertyType
-    {
-        Boolean,
-        String,
-        UInt32
-    }
-
     /// <summary> Defines all valid properties for the program. These properties may be applied to GameObjects, Rooms, etc. </summary>
     internal static class PropertyValidation 
     {
