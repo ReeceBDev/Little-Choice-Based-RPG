074a242b8a1a326f0f55aa68164cb7c568ab9d76
ReeceBDev <reece.bowen99@gmail.com>
2025-04-01 21:23:48 +0100

    Each seperate string in the user interface element is now organised into the ExploreMenuTextIdentity struct. This allows additional information to be packaged into each read.

---
+++ Little Choice-Based RPG/Managers/Player Manager/Frontend/UserInterface/UserInterfaceStrategies/NumberedConsole/ExploreMenuTextEntry.cs
@@ -0,0 +1,33 @@
+﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface.UserInterfaceStrategies.NumberedConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface.UserInterfaceStrategies.NumberedConsole
+{
+    public struct ExploreMenuTextEntry
+    {
+        static List<ExploreMenuIdentity> usedIdentifiers = new List<ExploreMenuIdentity>();
+        public ExploreMenuTextEntry(ExploreMenuIdentity setIdentifier, string setContent, uint setWriteSpeed)
+        {
+            AddIdentifier(setIdentifier);
+            Content = setContent;
+            WriteSpeed = setWriteSpeed;
+        }
+
+        private void AddIdentifier(ExploreMenuIdentity prospectiveIdentifier)
+        {
+            if (usedIdentifiers.Contains(prospectiveIdentifier))
+                throw new ArgumentException($"This ExploreMenuIdentifier of {prospectiveIdentifier} is already in use! Only one of each unique Identifier is allowed, except None. Try setting to {ExploreMenuIdentity.None}, since they are unlimited.");
+
+            usedIdentifiers.Add(prospectiveIdentifier);
+            UniqueIdentity = prospectiveIdentifier;
+        }
+
+        public ExploreMenuIdentity UniqueIdentity { get; private set; } // Can only use one of each, across all instances UserInterfaceTextEntry, except None, which can be used forever.
+        public string Content { get; init; }
+        public uint WriteSpeed { get; init; }
+    }
+}
