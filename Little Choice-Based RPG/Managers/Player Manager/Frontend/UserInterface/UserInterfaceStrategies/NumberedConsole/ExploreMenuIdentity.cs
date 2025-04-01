88f8b77c2f7ca0d91b141f56d6b293dfd0c6380c
ReeceBDev <reece.bowen99@gmail.com>
2025-04-01 21:22:55 +0100

    Created enums for splitting user interface elements up on the ExploreMenu. This enforces a consistent naming scheme for each element.

---
+++ Little Choice-Based RPG/Managers/Player Manager/Frontend/UserInterface/UserInterfaceStrategies/NumberedConsole/ExploreMenuIdentity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface.UserInterfaceStrategies.NumberedConsole
+{
+    public enum ExploreMenuIdentity
+    {
+        None, //Default, used by StyleLine
+        TopStatusBar,
+        TransitionalAction,
+        CurrentDescription,
+        HistoryLog,
+        AvailableChoices
+    }
+}
