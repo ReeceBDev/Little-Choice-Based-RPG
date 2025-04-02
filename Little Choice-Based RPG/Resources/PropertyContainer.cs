4665afac5c791c8df6ab93b2d1e2e48c396e60eb
ReeceBDev <reece.bowen99@gmail.com>
2025-04-02 22:48:28 +0100

    Added new PropertyContainer to act as a base parent for both GameObject and Room, providing unique identifier and the properties list.

---
+++ Little Choice-Based RPG/Resources/PropertyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources
+{
+    public class PropertyContainer
+    {
+        public PropertyHandler entityProperties = new PropertyHandler();
+
+        private protected PropertyContainer()
+        {
+            static UniqueID = ++globalCounter;
+        }
+    }
+}
