7154300d08ad615169f20f0c481c9debabe6dbcd
ReeceBDev <reece.bowen99@gmail.com>
2025-04-02 22:46:00 +0100

    Added new elements in order to support multiple parameters in practice.

--- Little Choice-Based RPG/Types/Interactions/Delegates/Interaction.cs
+++ Little Choice-Based RPG/Types/Interactions/Delegates/Interaction.cs
@@ -1,4 +1,5 @@
-﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
+using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
 using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,8 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractDelegate
     /// <summary> Provides a way to present options and choices to the player by exposing a delegate with pre-defined parameters. </summary>
     public abstract class Interaction : IInvokableInteraction
     {
+        protected IUserInterface? invocationMutexIdentity;
+
         static Interaction()
         {
             AddSelfIntoDelegateValidation();
@@ -38,18 +41,19 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractDelegate
         i.e. DelegateValidation.CreateValidDelegate("InteractUsingTargetObject", [DelegateParameter.Target_GameObject]);
             */
         }
-
-        /// <summary> Invokes the delegate using its required parameters. </summary>
-        public abstract void Invoke();
-
+        /// <summary> Invokes the delegate if able. Requests required parameters if unable. </summary>
+        public abstract void AttemptInvoke(IUserInterface setInvocationMutexIdentity);
+        public abstract void CancelInteraction(IUserInterface setInvocationMutexIdentity);
+        public abstract void GiveRequiredParameter(object newParameter, IUserInterface setInvocationMutexIdentity);
+        protected abstract void Invoke(IUserInterface setInvocationMutexIdentity);
 
         /// <summary> The title shown when a player gets listed their choice options. </summary>
-        public SanitizedString InteractionTitle { get; private protected set; } = new SanitizedString(string.Empty);
+        public SanitizedString InteractionTitle { get; init; } = new SanitizedString(string.Empty);
 
         /// <summary> The descriptor shown after a player selected their choice. </summary>
-        public SanitizedString InteractDescriptor { get; private protected set; } = new SanitizedString(string.Empty);
+        public SanitizedString InteractDescriptor { get; init; } = new SanitizedString(string.Empty);
 
         /// <summary> Describes how an Interaction should be presented by the User Interface, for example, if it belongs to a context-menu. </summary>
-        public InteractionRole InteractionContext { get; private protected set; }
+        public InteractionRole InteractionContext { get; init; }
     }
 }
