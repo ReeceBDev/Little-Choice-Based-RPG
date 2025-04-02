c4cf538eceeba58cf9cb49b4268b159dac0b55f4
ReeceBDev <reece.bowen99@gmail.com>
2025-04-02 22:47:00 +0100

    Renamed invoke to AttemptInvoke in order to invoke later elsewhere. The public access is AttemptInvoke, which is then able to request further information from the user as required, and once completed, finalise the invoke. Attempt invoke also sets the mutex until the entire process has completed.

--- Little Choice-Based RPG/Types/Interactions/Delegates/IInvokableInteraction.cs
+++ Little Choice-Based RPG/Types/Interactions/Delegates/IInvokableInteraction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,17 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractDelegate
     public interface IInvokableInteraction
     {
         /// <summary> Invokes the delegate using its required parameters. </summary>
-        void Invoke();
-
+        public void AttemptInvoke(IUserInterface setInvocationMutexIdentity);
+        public void CancelInteraction(IUserInterface setInvocationMutexIdentity);
+        public void GiveRequiredParameter(object newParameter, IUserInterface setInvocationMutexIdentity);
 
         /// <summary> The title shown when a player gets listed their choice options. </summary>
-        public SanitizedString InteractionTitle { get; private protected set; }
+        public SanitizedString InteractionTitle { get; init; }
 
         /// <summary> The descriptor shown after a player selected their choice. </summary>
-        public SanitizedString InteractDescriptor { get; private protected set; }
+        public SanitizedString InteractDescriptor { get; init; }
 
         /// <summary> Describes how an Interaction should be presented by the User Interface, for example, if it belongs to a context-menu. </summary>
-        public InteractionRole InteractionContext { get; private protected set; }
+        public InteractionRole InteractionContext { get; init; }
     }
 }
