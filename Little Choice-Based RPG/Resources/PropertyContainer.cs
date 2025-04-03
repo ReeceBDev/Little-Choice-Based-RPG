091fa08996fe20267dca878382146df8b39ab6a4
ReeceBDev <reece.bowen99@gmail.com>
2025-04-03 22:07:32 +0100

    Defined base functionality for PropertyContainer, which exposes methods for adding additional properties to derived classes.

--- Little Choice-Based RPG/Resources/PropertyContainer.cs
+++ Little Choice-Based RPG/Resources/PropertyContainer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,61 @@ namespace Little_Choice_Based_RPG.Resources
     public class PropertyContainer
     {
         public PropertyHandler entityProperties = new PropertyHandler();
+        protected static uint globalCounter;
 
-        private protected PropertyContainer()
+        private static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            static UniqueID = ++globalCounter;
+            { "ID", PropertyType.UInt32 },
+        };
+
+        private static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            {"ID", ++globalCounter},
+        };
+
+        static PropertyContainer()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected PropertyContainer(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            //Store the final list of properties
+            entityProperties = derivedProperties;
+
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private protected static void DeclareNewPropertyTypes(Dictionary<string, PropertyType> newProperties)
+        {
+            //Define required ValidProperties
+            foreach (var property in newProperties)
+                PropertyValidation.CreateValidProperty(property.Key, property.Value);
+        }
+
+        private protected static void ApplyDefaultProperties(PropertyHandler derivedProperties, Dictionary<string, object> defaultProperties)
+        {
+            foreach (KeyValuePair<string, object> property in defaultProperties)
+                if (!derivedProperties.HasExistingPropertyName(property.Key))
+                    derivedProperties.UpsertProperty(property.Key, property.Value);
+        }
+
+        private protected void ValidateRequiredProperties(Dictionary<string, PropertyType> testRequiredProperties)
+        {
+            foreach (var property in testRequiredProperties)
+                if (!entityProperties.HasExistingPropertyName(property.Key))
+                    throw new Exception($"A required property has not been defined in {entityProperties}. Property name: {property.Key}");
         }
     }
 }
