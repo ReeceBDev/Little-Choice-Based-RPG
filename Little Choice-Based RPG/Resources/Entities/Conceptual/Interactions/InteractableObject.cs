fabab5152c7d4dbf4ebbed3430bcc8406dcaaac4
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 03:23:19 +0100

    Converted interactableobject to new property system.

--- Little Choice-Based RPG/Resources/Entities/Conceptual/Interactions/InteractableObject.cs
+++ Little Choice-Based RPG/Resources/Entities/Conceptual/Interactions/InteractableObject.cs
@@ -1,6 +1,7 @@
 ﻿using Little_Choice_Based_RPG.Resources.Choices;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Descriptive_Derivatives;
 using Little_Choice_Based_RPG.Types;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,41 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions
 {
     public abstract class InteractableObject : DescriptiveObject
     {
-        public InteractableObject(string setName, string newGenericDescriptor, string newInspectDescriptor, decimal setWeightInKG = 0m)
-    : base(setName, newGenericDescriptor, newInspectDescriptor, setWeightInKG)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
 
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+
+        };
+
+        static InteractableObject()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected InteractableObject(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
         }
     }
 }
