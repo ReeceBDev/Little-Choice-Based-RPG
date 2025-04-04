611354be231b0dedd0283fad8d7219c4bb2ed50d
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 03:00:15 +0100

    Converted RoomDirection to extend the functionality of the new PropertyContainer base parent.

--- Little Choice-Based RPG/Resources/Entities/Immaterial/Transition/RoomDirection.cs
+++ Little Choice-Based RPG/Resources/Entities/Immaterial/Transition/RoomDirection.cs
@@ -11,32 +11,65 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Immaterial.Transition
 {
     public class RoomDirection : Immaterial
     {
-        private string defaultDescriptor = $"Feet scraping flecks of shrubbery drowned in dust scrambling from cracked sandstone, you forge your way further across the desolation.";
-        public RoomDirection(string setDirectionName, uint setDestinationRoomID, string setTravelDescriptor = "", uint setDependentObjectID = 0) : base(setDirectionName)
+
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            DestinationRoomID = setDestinationRoomID;
-            TravelDescriptor = (setTravelDescriptor == "") ? defaultDescriptor : setTravelDescriptor;
+            {"Direction.IsVisible", PropertyType.Boolean},
+            {"Direction.DestinationRoomID", PropertyType.UInt32 },
 
-            if (setDependentObjectID > 0)
-            {
-                DirectionIsVisible = false;
-                DependentObjectID = setDependentObjectID;
-            }
+            {"Descriptor.Direction.Travelling", PropertyType.String}
+        };
 
-            entityProperties.CreateProperty("IsImmaterial", true);
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+            {"Direction.AssociatedObjectID", PropertyType.UInt32},
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            {"Direction.IsVisible", true},
+            {"Descriptor.Direction.Travelling", "Feet scraping flecks of shrubbery drowned in dust scrambling from cracked sandstone, you forge your way further across the desolation."}
+        };
+
+        static RoomDirection()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
         }
 
-        public uint GetDestinationID(GameObject? usingKey = null) // Returns 0 if the key does not match DependentObjectID. usingKey should be the calling object.
+        private protected RoomDirection(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
         {
-            uint keyID = (usingKey != null) ? usingKey.ID : 0;
+            // When the direction is associated with an object, make it invisible by default.
+            // This forces the implementing object to reveal the direction when accessible, i.e. a Door being open.
+            if (entityProperties.HasExistingPropertyName("Direction.AssociatedObjectID"))
+                entityProperties.UpsertProperty("Direction.IsVisible", false);
 
-            return (keyID == DependentObjectID) ? DestinationRoomID : 0;
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
         }
 
-        
-        public string TravelDescriptor { get; private set; }
-        public bool DirectionIsVisible { get; private set; } = true;
-        public uint DependentObjectID { get; private set; } = 0; //0 must be Invalid because there is no objectID that can be 0.
-        private uint DestinationRoomID { get; set; }
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+
+
+        public uint GetDestinationID(uint? associatedObjectID = null) // associatedObjectID should be the calling object. Throws an exception if the associatedObjectID does not match the one stored on this direction in entityProperties.
+        {
+            //If no associated object is required, return 
+            if (!entityProperties.HasExistingPropertyName("Direction.AssociatedObjectID"))
+                return (uint) entityProperties.GetPropertyValue("Direction.DestinationRoomID");
+
+            //Check that the associated object matches the one stored in entityProperties
+            if (associatedObjectID != (uint)entityProperties.GetPropertyValue("Direction.AssociatedObjectID"))
+                throw new Exception("The associated object ID did not match the one stored in entityProperties!");
+
+            return (uint)entityProperties.GetPropertyValue("Direction.DestinationRoomID");
+        }
     }
 }
