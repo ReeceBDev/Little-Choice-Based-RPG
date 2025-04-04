23d5a0378340b28d25618809ae40f698b0e40093
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 03:22:58 +0100

    Converted MovingObject to new property system. Commented out unused functions.

--- Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/MovingObject.cs
+++ Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/MovingObject.cs
@@ -1,4 +1,5 @@
 ﻿using Little_Choice_Based_RPG.Resources.Rooms;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,50 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_De
 {
     public class MovingObject : WeightbearingObject
     {
-        private protected MovingObject(string setName, decimal setWeightInKG = 0m, decimal setStrengthInKG = 0m)
-            : base(setName, setWeightInKG, setStrengthInKG)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
 
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+
+        };
+
+        static MovingObject()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
         }
-        
+
+        private protected MovingObject(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+
+        /*
         public void Move(uint newPosition)
         {
             //this.Position = newPosition;
         }
 
         //public void MoveToRoom(RoomDirection direction) => this.Position = direction.DestinationRoomID;
+        */
     }
 }
