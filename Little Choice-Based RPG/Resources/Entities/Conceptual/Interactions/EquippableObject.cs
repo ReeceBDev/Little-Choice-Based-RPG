0bf30f132cdabdd407ee9bd797948cc89fe9b2da
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 03:23:57 +0100

    Moved properties out of PropertyValidation to their relevant classes.  Converted descriptiveobject and equippableobject to new property system and expanded class properties to cover the new descriptors.

--- Little Choice-Based RPG/Resources/Entities/Conceptual/Interactions/EquippableObject.cs
+++ Little Choice-Based RPG/Resources/Entities/Conceptual/Interactions/EquippableObject.cs
@@ -1,5 +1,6 @@
 ﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Descriptive_Derivatives;
 using Little_Choice_Based_RPG.Types;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,42 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions
 {
     public abstract class EquippableObject : InteractableObject
     {
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+
+        };
+
+        static changeme()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected changeme(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
         public EquippableObject(string setName, string newGenericDescriptor, string newInspectDescriptor, decimal setWeightInKG = 0m)
 : base(setName, newGenericDescriptor, newInspectDescriptor, setWeightInKG)
         {
