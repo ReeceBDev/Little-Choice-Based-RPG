b17efae0ec3a94f35adce9e05e0e5d3567412484
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 02:59:25 +0100

    Converted Immaterial to be compatible with extending functionality of the new PropertyContainer base class.

--- Little Choice-Based RPG/Resources/Entities/Immaterial/Immaterial.cs
+++ Little Choice-Based RPG/Resources/Entities/Immaterial/Immaterial.cs
@@ -1,4 +1,5 @@
 ﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,44 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Immaterial
 {
     public class Immaterial : GameObject
     {
-        public Immaterial(string setName) : base(setName)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            entityProperties.CreateProperty("IsImmaterial", true);
+            {"IsImmaterial", PropertyType.Boolean},
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            { "IsImmaterial", true }
+        };
+
+        static Immaterial()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected Immaterial(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        {
+            //Freeze IsImmaterial property.
+            entityProperties.FreezeProperty("IsImmaterial");
+
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
         }
     }
 }
