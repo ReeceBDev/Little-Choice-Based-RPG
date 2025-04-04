9a897f8b5a0e59bce19cb8675d5d93c5202d32cc
ReeceBDev <reece.bowen99@gmail.com>
2025-04-04 16:56:41 +0100

    Outlined flammability system

---
+++ Little Choice-Based RPG/Resources/Systems/Damage/Flammability/FlammabilitySystem.cs
@@ -0,0 +1,35 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Flammability
+{
+    public class FlammabilitySystem
+    {
+        private GameObject parentObject;
+        static FlammabilitySystem()
+        {
+            PropertyValidation.CreateValidProperty("IsFlammable", PropertyType.Boolean);
+            PropertyValidation.CreateValidProperty("Flammability.IsBurning", PropertyType.Boolean);
+            PropertyValidation.CreateValidProperty("Flammability.IsBurnt", PropertyType.Boolean);
+        }
+
+        public FlammabilitySystem(GameObject instantiatingObject)
+        {
+            parentObject = instantiatingObject;
+        }
+
+        public void SetAflame()
+        {
+            // This class needs to be event driven :)
+        }
+        public void Extinguish()
+        {
+
+        }
+    }
+}
