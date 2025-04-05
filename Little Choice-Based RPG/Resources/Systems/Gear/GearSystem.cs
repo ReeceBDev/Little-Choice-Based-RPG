5d127dbb5c2734127cf9199d65972b1ef5faedf1
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:10:05 +0100

    Created GearSystem to represent the former PlayerGear struct. GearSystem uses the new entity properties system to define gear slots and what they are occupied with.

---
+++ Little Choice-Based RPG/Resources/Systems/Gear/GearSystem.cs
@@ -0,0 +1,19 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Gear
+{
+    internal class GearSystem
+    {
+        static GearSystem()
+        {
+            PropertyValidation.CreateValidProperty("HasGearSlots", PropertyType.Boolean);
+            PropertyValidation.CreateValidProperty("Gear.Slot.Helmet.ID", PropertyType.UInt32);
+        }
+    }
+}
