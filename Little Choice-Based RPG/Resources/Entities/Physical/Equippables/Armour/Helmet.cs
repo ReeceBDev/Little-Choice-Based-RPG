ec3a8ae11c3611fee30919445977b93963b72a0d
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:16:07 +0100

    Helmet uses the new entity property system.

--- Little Choice-Based RPG/Resources/Entities/Physical/Equippables/Armour/Helmet.cs
+++ Little Choice-Based RPG/Resources/Entities/Physical/Equippables/Armour/Helmet.cs
@@ -5,15 +5,48 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 
 namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour
 {
-    internal abstract class Helmet : EquippableObject
+    internal class Helmet : EquippableObject
     {
-        private protected Helmet(string setName, string newGenericDescriptor, string newInspectDescriptor, decimal setWeightInKG = 0m)
-: base(setName, newGenericDescriptor, newInspectDescriptor, setWeightInKG)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            WeightInKG = 1.8m;
+
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            {"WeightInKG", 2.3m},
+            {"Descriptor.Generic.Default", "Folorn, a mudcaked helmet rots here in its own scrap, long since abandoned." }
+        };
+
+        static Helmet()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        public Helmet(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
         }
     }
 }
