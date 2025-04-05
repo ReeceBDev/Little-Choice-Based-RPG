8e0447bd781e8b389910de8c7fce335d763bb2a9
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:18:53 +0100

    LivingCreature uses new entityproperties system.

--- Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/LivingCreature.cs
+++ Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/LivingCreature.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +11,43 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_De
 {
     public class LivingCreature : MovingObject
     {
-        private protected bool isAlive;
-        private protected int health;
-        private protected LivingCreature(string setName, decimal setWeightInKG = 0m, decimal setStrengthInKG = 0m)
-            : base(setName, setWeightInKG, setStrengthInKG)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            isAlive = true;
-            health = 100;
+            { "IsLiving", PropertyType.Boolean },
+            { "Health", PropertyType.UInt32 },
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            { "IsLiving", true },
+            { "Health", 100u },
+        };
+
+        static LivingCreature()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected LivingCreature(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
         }
     }
 }
