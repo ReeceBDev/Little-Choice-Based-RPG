d0a3f10feeab7366943d8a9f5ecd10961900f1ad
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:08:11 +0100

    Major refactor for player. Uses the entity properties system. Contains the new code for creating an instance of davodianMk1Helmet. This code will later be moved to an independent repository, but currently works as a proof of concept.

--- Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
+++ Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
@@ -8,71 +8,80 @@ using System.Threading.Tasks;
 using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
 using Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour;
-using Little_Choice_Based_RPG.Resources.Choices;
 using Little_Choice_Based_RPG.Resources.Rooms;
-using Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour.Helmets;
 using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_Derivatives;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Descriptive_Derivatives;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 
 namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
 {
-    internal struct PlayerGear
-    {
-        internal Helmet equippedHelmet;
-    }
     public class Player : LivingCreature
     {
-        private protected PlayerGear carriedGear;
-
-        private protected bool wearingHelmet = true;
-
-        private protected bool playerCanHear = false;
-        private protected bool playerCanSee = false;
-        private protected bool playerCanMove = false;
-        private protected bool isPlayerKnockedDown = true;
-
-        public uint Position;
-
-        public Player(string setName, uint setPosition, decimal setWeightInKG = 0m, decimal setStrengthInKG = 0m) : base(setName, setWeightInKG, setStrengthInKG = 0m)
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
-            Position = setPosition;
-            //carriedGear.equippedHelmet = new DavodianMkIHelmet(Position);
+            {"Player.CanHear", PropertyType.Boolean },
+            {"Player.CanSee", PropertyType.Boolean }
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            {"HasGearSlots", true },
+            {"Player.CanHear", true},
+            {"Player.CanSee", true },
+            {"StrengthInKG",  32.0m}
+        };
+
+        static Player()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
         }
 
-        public void Equip()
+        public Player(uint setSpawnRoomID, Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
         {
-            if (carriedGear.equippedHelmet == null)
-            {
-                LastActionDescriptor.Value = carriedGear.equippedHelmet.EquipDescriptor;
-                wearingHelmet = true;
-            }
-            else
-                LastActionDescriptor.Value = $"The {GetEquippedHelmet()} that you wear prevents you from doing this.";
+            //Set player spawn position
+            entityProperties.UpsertProperty("Position", setSpawnRoomID);
 
+            //Give the player a helmet
+            Dictionary<string, object> davodianMk1Helmet = new Dictionary<string, object>();
+            davodianMk1Helmet.Add("Name", "Davodian MkI Covered Faceplate");
+            davodianMk1Helmet.Add("Descriptor.Generic.Default", "Discarded aside here is your original Davodian MkI, its dented gunmetal grey faceplate long lustreless.");
+            davodianMk1Helmet.Add("Descriptor.Inspect.Default", "Disfigured and mottled from years of abuse, the gunmetal grey protective sensors relay a live feed, protecting your vision and perceptible hearing.\r\nThe units bodywork has been long since reworked in patchwork copper shielding - under the seams near-charred components made up of scrapped debris poke through residual sand.");
+            davodianMk1Helmet.Add("Descriptor.Equip", "Dusty residue coats your hands as you heave the thick, heavy metal above you and press your forehead in.\r\nInterlocking clicks engage behind your neck, a gentle buzz as the metal comes online.");
+            davodianMk1Helmet.Add("Descriptor.Unequip", "Engaging the clasp at the rear, the locks reluctantly scrape their disengaging clicks and the full weight of the visor bears down on your head.\r\nSandpaper lining scratches the sides of your face when you tilt your head forwards and force the faceplate off.");
+
+            davodianMk1Helmet.Add("Repair.IsRepairable", true);
+            davodianMk1Helmet.Add("Repair.IsRepairableByChoice", true);
+            davodianMk1Helmet.Add("Descriptor.Repair.Choice.Interact", "Repair - Re - calibrate the helmets longitudinal wave sensor - array.");
+            davodianMk1Helmet.Add("Descriptor.Repair.Choice.Repairing", "You fixed the helmet, yayy!");
+
+            davodianMk1Helmet.Add("IsBreakable", true);
+            davodianMk1Helmet.Add("IsBreakableByChoice", true);
+            davodianMk1Helmet.Add("Descriptor.Choice.Break.Interact", "Damage - Intentionally misalign the helmets longitudinal wave sensor-array");
+            davodianMk1Helmet.Add("Descriptor.Descriptor.Choice.Breaking", "You broke the helmet oh nooo!");
+            Helmet testDavodian = new Helmet(davodianMk1Helmet);
+
+            this.entityProperties.UpsertProperty("Gear.Slot.Helmet.ID", testDavodian.entityProperties.GetPropertyValue("ID"));
+            testDavodian.Equip();
+
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
         }
 
-        public void Unequip()
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
         {
-            if (carriedGear.equippedHelmet == null)
-            {
-                LastActionDescriptor.Value = "You reach your hands to your head and quickly realise you aren't wearing a helmet.";
-            }
-            else
-            {
-                LastActionDescriptor.Value = carriedGear.equippedHelmet.UnequipDescriptor;
-                wearingHelmet = false;
-            }
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
 
-        }
-
-        public virtual List<Choice> GenerateChoices()
-        {
-            List<Choice> choices = new List<Choice>();
-
-            // choices.AddRange(HandleDirectionChoices());
-
-            return choices;
+            return derivedProperties; //Return is required to give (base) the derived list.
         }
 
         /*
@@ -108,11 +117,5 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
             return movementDescriptor;
         }
         */
-
-        public SanitizedString GetEquippedHelmet() => carriedGear.equippedHelmet.Name;
-
-        public SanitizedString PlayerDescriptor { get; private set; }
-        public SanitizedString LastActionDescriptor { get; private set; }
-        public bool CanHear => playerCanHear;
     }
 }
