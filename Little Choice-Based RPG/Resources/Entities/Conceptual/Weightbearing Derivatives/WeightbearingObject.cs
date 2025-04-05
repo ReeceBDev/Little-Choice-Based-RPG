7dcbef51021c95b11992b47c92b70ed4c75f2d42
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:22:17 +0100

    Changed type for the StrengthInKg and TotalWeightHeldInKG properties to be decimal instead of uint to better reflect the appearance of kilogrammes. Moved TotalWeightHeldInKG to optional, and made it be added if it doesnt already exist.

--- Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/WeightbearingObject.cs
+++ Little Choice-Based RPG/Resources/Entities/Conceptual/Weightbearing Derivatives/WeightbearingObject.cs
@@ -14,13 +14,12 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_De
         private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
             {"IsWeightBearing", PropertyType.Boolean},
-            {"StrengthInKG", PropertyType.UInt32}, // maximum weightbearing capacity
-            {"TotalWeightHeldInKG", PropertyType.UInt32}, //current weight held
+            {"StrengthInKG", PropertyType.Decimal}, // maximum weightbearing capacity
         };
 
         private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
         {
-
+            {"TotalWeightHeldInKG", PropertyType.Decimal}, //current weight held
         };
 
         private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
@@ -35,14 +34,14 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_De
             DeclareNewPropertyTypes(optionalProperties);
         }
 
-        private protected WeightbearingObject(PropertyHandler? derivedProperties = null)
-            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        private protected WeightbearingObject(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
         {
             //Validate required properties have been set on entityProperties
             ValidateRequiredProperties(requiredProperties);
         }
 
-        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
         {
             //Apply default properties for this class to the current list of derivedProperties
             ApplyDefaultProperties(derivedProperties, defaultProperties);
@@ -53,6 +52,9 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_De
         {
             if (!entityProperties.HasProperty("IsWeightBearing", true))
                 throw new Exception($"{this} is unable to hold any weight itself!");
+
+            if (!entityProperties.HasExistingPropertyName("TotalWeightHeldInKG"))
+                entityProperties.CreateProperty("TotalWeightHeldInKG", 0);
 
             uint totalStrength = (uint)entityProperties.GetPropertyValue("StrengthInKG");
             uint weightHeld = (uint)entityProperties.GetPropertyValue("TotalWeightHeldInKG");
