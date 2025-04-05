ea2c526f684475002a0ce35883bc64055c800059
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:06:07 +0100

    Converted chair to use the new entityProperty system

--- Little Choice-Based RPG/Resources/Entities/Physical/Furniture/Chair.cs
+++ Little Choice-Based RPG/Resources/Entities/Physical/Furniture/Chair.cs
@@ -2,6 +2,7 @@
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Descriptive_Derivatives;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,47 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Furniture
 {
     public class Chair : InteractableObject
     {
+
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+
+        };
+
+        static changeme()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected changeme(PropertyHandler? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new PropertyHandler()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static PropertyHandler SetLocalProperties(PropertyHandler derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
         public Chair(string setName, string newGenericDescriptor, string newInspectDescriptor, decimal setWeightInKG = 0m)
     : base(setName, newGenericDescriptor, newInspectDescriptor, setWeightInKG)
         {
-            entityProperties.CreateProperty("IsBurnt", false);
+            entityProperties.CreateProperty("IsBurnt", false); // Put into IsFlammable, FireSystem class?
         }
 
         public override List<Choice> GenerateChoices()
