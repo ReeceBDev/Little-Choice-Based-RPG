e28d1fd7a313cee2185abbb47298965db3125b7d
ReeceBDev <reece.bowen99@gmail.com>
2025-04-05 04:18:06 +0100

    New GameObjectFactory class to dynamically instantiate objects based on the type defined within their properties.

---
+++ Little Choice-Based RPG/Managers/World/GameObjectFactory.cs
@@ -0,0 +1,26 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Managers.World
+{
+    /// <summary> Manufactures GameObjects of any type based on its properties. Gets allocated an outline of each object from JSONs. </summary>
+    internal class GameObjectFactory
+    {
+        public object NewGameObject(Dictionary<string, object> properties)
+        {
+            if (!properties.ContainsKey("Type"))
+                throw new ArgumentException("The properties of the new object did not include the Type property! Properties: {properties}");
+
+            Type objectType = Type.GetType(properties["Type"].ToString());
+
+            object newGameObject = Activator.CreateInstance(objectType);
+
+            return newGameObject;
+        }
+    }
+}
