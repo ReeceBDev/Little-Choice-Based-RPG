18e6164996bff3aac052d4f8c5dfa411ddde723e
ReeceBDev <reece.bowen99@gmail.com>
2025-04-06 22:02:19 +0100

    Moved the helmet declaration outside of the player, leaving an ID in its place.

--- Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
+++ Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
@@ -44,34 +44,9 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
             DeclareNewPropertyTypes(optionalProperties);
         }
 
-        public Player(uint setSpawnRoomID, Dictionary<string, object>? derivedProperties = null)
+        public Player(Dictionary<string, object>? derivedProperties = null)
             : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
         {
-            //Set player spawn position
-            entityProperties.UpsertProperty("Position", setSpawnRoomID);
-
-            //Give the player a helmet
-            Dictionary<string, object> davodianMk1Helmet = new Dictionary<string, object>();
-            davodianMk1Helmet.Add("Name", "Davodian MkI Covered Faceplate");
-            davodianMk1Helmet.Add("Descriptor.Generic.Default", "Discarded aside here is your original Davodian MkI, its dented gunmetal grey faceplate long lustreless.");
-            davodianMk1Helmet.Add("Descriptor.Inspect.Default", "Disfigured and mottled from years of abuse, the gunmetal grey protective sensors relay a live feed, protecting your vision and perceptible hearing.\r\nThe units bodywork has been long since reworked in patchwork copper shielding - under the seams near-charred components made up of scrapped debris poke through residual sand.");
-            davodianMk1Helmet.Add("Descriptor.Equip", "Dusty residue coats your hands as you heave the thick, heavy metal above you and press your forehead in.\r\nInterlocking clicks engage behind your neck, a gentle buzz as the metal comes online.");
-            davodianMk1Helmet.Add("Descriptor.Unequip", "Engaging the clasp at the rear, the locks reluctantly scrape their disengaging clicks and the full weight of the visor bears down on your head.\r\nSandpaper lining scratches the sides of your face when you tilt your head forwards and force the faceplate off.");
-
-            davodianMk1Helmet.Add("Repair.IsRepairable", true);
-            davodianMk1Helmet.Add("Repair.IsRepairableByChoice", true);
-            davodianMk1Helmet.Add("Descriptor.Repair.Choice.Interact", "Repair - Re - calibrate the helmets longitudinal wave sensor - array.");
-            davodianMk1Helmet.Add("Descriptor.Repair.Choice.Repairing", "You fixed the helmet, yayy!");
-
-            davodianMk1Helmet.Add("IsBreakable", true);
-            davodianMk1Helmet.Add("IsBreakableByChoice", true);
-            davodianMk1Helmet.Add("Descriptor.Choice.Break.Interact", "Damage - Intentionally misalign the helmets longitudinal wave sensor-array");
-            davodianMk1Helmet.Add("Descriptor.Descriptor.Choice.Breaking", "You broke the helmet oh nooo!");
-            Helmet testDavodian = new Helmet(davodianMk1Helmet);
-
-            this.entityProperties.UpsertProperty("Gear.Slot.Helmet.ID", testDavodian.entityProperties.GetPropertyValue("ID"));
-            testDavodian.Equip();
-
             //Validate required properties have been set on entityProperties
             ValidateRequiredProperties(requiredProperties);
         }
