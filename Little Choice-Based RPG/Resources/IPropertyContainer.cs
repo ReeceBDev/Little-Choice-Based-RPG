aba813d4c8c03a73322ae9240c9a18df033055a2
ReeceBDev <reece.bowen99@gmail.com>
2025-04-06 21:57:13 +0100

    Created IPropertyContainer interface for providing known access to Properties and a new SubscribeToSystem method, which will send an alert to systems that the object wants to subscribe to them.

---
+++ Little Choice-Based RPG/Resources/IPropertyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources
+{
+    internal interface IPropertyContainer
+    {
+        public PropertyHandler Properties { get; set; }
+
+        public void SubscribeToSystem(string systemReferenceName);
+    }
+}
