da4638bada77e5f66b68a7f45f8749371b1da843
ReeceBDev <reece.bowen99@gmail.com>
2025-04-06 21:59:39 +0100

    Created SystemSubscriptionEventBus to allow systems to listen for subscription events on new, unheard of objects upon generation from within the GameObjectFactory.cs

---
+++ Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionEventBus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.SystemEventBus
+{
+    public struct SystemSubscriptionRequestEventArgs(object targetObject, string systemReferenceName);
+    public class SystemSubscriptionEventBus
+    {
+        public event EventHandler<SystemSubscriptionRequestEventArgs> SystemSubcriptionRequest;
+
+        public void Subscribe(object targetObject, string systemReferenceName)
+        {
+            SystemSubcriptionRequest(this, new SystemSubscriptionRequestEventArgs(targetObject, systemReferenceName));
+        }
+    }
+}
