0b72393a27425f90a03dd5ec18082510db36a84b
ReeceBDev <reece.bowen99@gmail.com>
2025-04-07 02:51:21 +0100

    New PropertyLogic class acts as a base for all existing property logic systems. This will allow new propertycontainer derived objects to subscribe to propertylogic systems and give base implementations, such as the population of choices on gameobjects based on properties.

---
+++ Little Choice-Based RPG/Resources/Systems/PropertyLogic.cs
@@ -0,0 +1,18 @@
+﻿using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems
+{
+    public class PropertyLogic
+    {
+        private SystemSubscriptionEventBus currentSystemSubscriptionEventBus;
+        public PropertyLogic(SystemSubscriptionEventBus setSystemSubscriptionEventBus)
+        {
+            currentSystemSubscriptionEventBus = setSystemSubscriptionEventBus;
+        }
+    }
+}
