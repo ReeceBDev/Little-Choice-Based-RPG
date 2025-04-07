7341fa241600a894cd84ff069ee12f9cabde6197
ReeceBDev <reece.bowen99@gmail.com>
2025-04-07 18:08:02 +0100

    Built PropertyLogic to create unique component property on instantiation which reference its derived classes. This unique property is used to subscribe PropertyContainers to those systems.
    Upon subscribing to a system, the system will now subscribe itself to the PropertyChanged event on each propertycontainers' PropertyHandler. This logic is performed within PropertyLogic.

--- Little Choice-Based RPG/Resources/Systems/PropertyLogic.cs
+++ Little Choice-Based RPG/Resources/Systems/PropertyLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,29 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Resources.Systems
 {
-    public class PropertyLogic
+    /// <summary> Provide basic access to property logic contained within systems derived from this class. </summary>
+    public abstract class PropertyLogic
     {
-        private SystemSubscriptionEventBus currentSystemSubscriptionEventBus;
-        public PropertyLogic(SystemSubscriptionEventBus setSystemSubscriptionEventBus)
+        public PropertyLogic(SystemSubscriptionEventBus systemSubscriptionEventBusReference)
         {
-            currentSystemSubscriptionEventBus = setSystemSubscriptionEventBus;
+            //Create unique components for each derived system
+            if (!(PropertyValidation.IsValidProperty($"Component.{this.GetType().Name.ToString()}", PropertyType.Boolean)))
+                PropertyValidation.CreateValidProperty($"Component.{this.GetType().Name.ToString()}", PropertyType.Boolean);
+
+            //Listen for new property containers which try to use systems
+            systemSubscriptionEventBusReference.SystemSubcriptionRequest += OnSystemSubscriptionRequest;
+        }
+
+        protected virtual void OnSystemSubscriptionRequest(object sender, SystemSubscriptionRequestEventArgs systemSubscriptionRequestData)
+        {
+            //If the current system type matches the reference name, subscribe to the requester's property list.
+            if (systemSubscriptionRequestData.systemReferenceName == this.GetType().Name.ToString())
+                systemSubscriptionRequestData.targetPropertyHandler.Properties.PropertyChanged += OnPropertyChanged;
+        }
+
+        protected virtual void OnPropertyChanged(object sender, EntityProperty updatedProperty)
+        {
+
         }
     }
 }
