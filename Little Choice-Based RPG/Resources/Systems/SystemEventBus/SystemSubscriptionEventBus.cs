ef4146f5dbcf7ef5dd3364ab6014224ec8e9aaa8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-07 02:50:17 +0100

    Made SystemSubscriptionEventBus singleton. Gave it OnSystemSubscriptionRequest method to handle the event.

--- Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionEventBus.cs
+++ Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionEventBus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Resources.Systems.Damage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,22 @@ namespace Little_Choice_Based_RPG.Resources.Systems.SystemEventBus
     public struct SystemSubscriptionRequestEventArgs(object targetObject, string systemReferenceName);
     public class SystemSubscriptionEventBus
     {
+        private static readonly SystemSubscriptionEventBus singletonInstance = new SystemSubscriptionEventBus();
+
         public event EventHandler<SystemSubscriptionRequestEventArgs> SystemSubcriptionRequest;
 
-        public void Subscribe(object targetObject, string systemReferenceName)
+        public void Subscribe(object targetObject, string systemReferenceName) =>
+            OnSystemSubscriptionRequest(targetObject, systemReferenceName);
+
+        private protected void OnSystemSubscriptionRequest(object targetObject, string systemReferenceName) =>
+            SystemSubcriptionRequest?.Invoke(this, new SystemSubscriptionRequestEventArgs(targetObject, systemReferenceName));
+
+        public static SystemSubscriptionEventBus Instance
         {
-            SystemSubcriptionRequest(this, new SystemSubscriptionRequestEventArgs(targetObject, systemReferenceName));
+            get
+            {
+                return singletonInstance;
+            }
         }
     }
 }
