65888323fd0bec068cc806c103e1a98bd97738f1
ReeceBDev <reece.bowen99@gmail.com>
2025-04-08 16:23:20 +0100

    Major bugfixes for PropertyHandler. - GetEntityProperty renamed to CopyEntityProperty to better reflect its capability. - New method, GetEntityPropertyIndex, replaces some instances where CopyEntityProperty was used. - Property updates now grab the index, delete and reinsert the new property at the same position, since these are value types on a list and not references.

--- Little Choice-Based RPG/Types/EntityProperties/PropertyHandler.cs
+++ Little Choice-Based RPG/Types/EntityProperties/PropertyHandler.cs
@@ -1,30 +1,43 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Types.EntityProperties
 {
+    public record struct PropertyChangedEventArgs(PropertyContainer sourceContainer, EntityProperty newProperty);
     public class PropertyHandler
     {
+        private PropertyContainer sourceContainer;
+        public PropertyHandler(PropertyContainer setSourceContainer)
+        {
+            sourceContainer = setSourceContainer;
+        }
+
         /// <summary> Creates a new property on this object. </summary>
         public void CreateProperty(string setPropertyName, object setPropertyValue, bool isReadOnly = false)
         {
-            EntityProperties.Add(new EntityProperty(setPropertyName, setPropertyValue, isReadOnly));
+            EntityProperty newProperty = new EntityProperty(setPropertyName, setPropertyValue, isReadOnly);
+            EntityProperties.Add(newProperty);
+
+            OnPropertyChanged(newProperty);
         }
 
         /// <summary> Updates a property on this object, which is not currently frozen. </summary>
-        public void UpdateProperty(string propertyName, object setPropertyValue, bool isReadOnly = false)
+        public void ReplaceProperty(string propertyName, object setPropertyValue, bool isReadOnly = false)
         {
             if (!IsPropertyReadOnly(propertyName))
             {
-                GetEntityProperty(propertyName).SetPropertyValue(setPropertyValue);
+                int index = GetEntityPropertyIndex(propertyName);
 
-                if (isReadOnly)
-                    FreezeProperty(propertyName);
-                // Thaw is not required, as it will do nothing and throw an exception anyway.
+                EntityProperties.RemoveAt(index);
+                EntityProperties.Insert(index, new EntityProperty(propertyName, setPropertyValue, isReadOnly));
+
+                OnPropertyChanged(CopyEntityProperty(propertyName)); //This creates a new copy because EntityProperty is a value type.
             }
             else 
                 throw new ArgumentException("Can't update this property. This EntityProperty is set to ReadOnly. (ReadOnly = True)");
@@ -34,7 +47,7 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
         public void UpsertProperty(string setPropertyName, object setPropertyValue, bool isReadOnly = false)
         {
             if (HasExistingPropertyName(setPropertyName))
-                UpdateProperty(setPropertyName, setPropertyValue, isReadOnly);
+                ReplaceProperty(setPropertyName, setPropertyValue, isReadOnly);
             else
                 CreateProperty(setPropertyName, setPropertyValue, isReadOnly);
         }
@@ -42,7 +55,7 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
         /// <summary> Removes a property from this Object. </summary>
         public void DeleteProperty(string propertyName) //Removes a property, unless frozen.
         {          
-            EntityProperty targetProperty = GetEntityProperty(propertyName);
+            EntityProperty targetProperty = CopyEntityProperty(propertyName);
             
             if (IsPropertyReadOnly(propertyName) == false)
                 EntityProperties.Remove(targetProperty);
@@ -55,7 +68,7 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
         public void FreezeProperty(string propertyName)
         {
             if (!IsPropertyReadOnly(propertyName))
-                GetEntityProperty(propertyName).SetPropertyAsReadOnly();
+                EntityProperties[GetEntityPropertyIndex(propertyName)].SetPropertyAsReadOnly();
             else
                 throw new ArgumentException("Can't freeze this property. This EntityProperty is already Frozen. (ReadOnly = True)");
         }
@@ -64,7 +77,7 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
         public void ThawProperty(string propertyName) // Sets a property to ReadOnly = False
         {
             if (!IsPropertyReadOnly(propertyName))
-                GetEntityProperty(propertyName).UnsetPropertyAsReadOnly();
+                EntityProperties[GetEntityPropertyIndex(propertyName)].UnsetPropertyAsReadOnly();
             else
                 throw new ArgumentException("Can't thaw this property. This EntityProperty is already Thawed. (ReadOnly = False)");
         }
@@ -75,7 +88,7 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
             foreach (EntityProperty testProperty in EntityProperties)
             {
                 if (testProperty.PropertyName == propertyName)
-                    if (testProperty.PropertyValue == propertyValue)
+                    if (testProperty.PropertyValue.Equals(propertyValue))
                         return true;
             }
 
@@ -95,15 +108,15 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
         }
 
         /// <summary> Returns a value from a matching property name on this object. </summary>
-        public object GetPropertyValue(string propertyName) => GetEntityProperty(propertyName).PropertyValue;
+        public object GetPropertyValue(string propertyName) => CopyEntityProperty(propertyName).PropertyValue;
 
 
         /// <summary> Checks if property is modifiable. </summary>
-        public bool IsPropertyReadOnly(string propertyName) => GetEntityProperty(propertyName).ReadOnly;
+        public bool IsPropertyReadOnly(string propertyName) => CopyEntityProperty(propertyName).ReadOnly;
 
 
-        /// <summary> Returns an EntityProperty from a matching property name on this object. </summary>
-        private EntityProperty GetEntityProperty(string propertyName)
+        /// <summary> Returns a reference to an EntityProperty from a matching property name on this object. </summary>
+        private EntityProperty CopyEntityProperty(string propertyName)
         {
             foreach (EntityProperty testProperty in EntityProperties)
             {
@@ -114,8 +127,22 @@ namespace Little_Choice_Based_RPG.Types.EntityProperties
             throw new ArgumentException("No such EntityProperty exists inside this objects list of EntityProperties.");
         }
 
-        public event EventHandler<EntityProperty> PropertyChanged;
-        protected private void OnPropertyChanged(EntityProperty updatedProperty) => PropertyChanged?.Invoke(this, updatedProperty);
+        private int GetEntityPropertyIndex(string propertyName)
+        {
+            if (!HasExistingPropertyName(propertyName))
+                throw new ArgumentException($"The name {propertyName} was not found on this object's EntityProperties {EntityProperties}!");
+
+            for (int index = 0; index < EntityProperties.Count(); index++)
+            {
+                if (EntityProperties[index].PropertyName == propertyName)
+                    return index;
+            }
+
+            throw new Exception("The index didn't match any names that were checked. This shouldn't have happened!");
+        }
+
+        public event EventHandler<PropertyChangedEventArgs> PropertyChanged;
+        protected private void OnPropertyChanged(EntityProperty updatedProperty) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(sourceContainer, updatedProperty));
         public List<EntityProperty> EntityProperties { get; private set; } = new List<EntityProperty>();
     }
 }
