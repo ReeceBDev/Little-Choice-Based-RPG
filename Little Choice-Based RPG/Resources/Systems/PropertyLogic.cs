1996f0cfea1b296cd4459d075c09df64abd4e9c0
ReeceBDev <reece.bowen99@gmail.com>
2025-04-08 16:27:47 +0100

    Since SystemSubscriptionEventBus is now static, removed instantiation inside PropertyLogic and replaced with static references. Added logic to OnSystemSubscriptionRequest which will populate an object freshly requesting a system with the existing and valid interactions it should have. This logic is encapsulated in a function named GiveInitialInteractions. Added new method GiveInteraction for sharing between derivatives. Added two new abstract methods, OnPropertyChanged, which is for sorting out which methods are run when whichever properties are changed, on each system, and GiveInitialInteractions, which populates a new system subscriber with its initial interactions.

--- Little Choice-Based RPG/Resources/Systems/PropertyLogic.cs
+++ Little Choice-Based RPG/Resources/Systems/PropertyLogic.cs
@@ -1,5 +1,6 @@
 ﻿using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types.EntityProperties;
+using Little_Choice_Based_RPG.Types.Interactions.InteractDelegate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,26 +12,38 @@ namespace Little_Choice_Based_RPG.Resources.Systems
     /// <summary> Provide basic access to property logic contained within systems derived from this class. </summary>
     public abstract class PropertyLogic
     {
-        public PropertyLogic(SystemSubscriptionEventBus systemSubscriptionEventBusReference)
+        public PropertyLogic()
         {
             //Create unique components for each derived system
             if (!(PropertyValidation.IsValidProperty($"Component.{this.GetType().Name.ToString()}", PropertyType.Boolean)))
                 PropertyValidation.CreateValidProperty($"Component.{this.GetType().Name.ToString()}", PropertyType.Boolean);
 
             //Listen for new property containers which try to use systems
-            systemSubscriptionEventBusReference.SystemSubcriptionRequest += OnSystemSubscriptionRequest;
+            SystemSubscriptionEventBus.SystemSubcriptionRequest += OnSystemSubscriptionRequest;
         }
-
+        
         protected virtual void OnSystemSubscriptionRequest(object sender, SystemSubscriptionRequestEventArgs systemSubscriptionRequestData)
         {
             //If the current system type matches the reference name, subscribe to the requester's property list.
             if (systemSubscriptionRequestData.systemReferenceName == this.GetType().Name.ToString())
-                systemSubscriptionRequestData.targetPropertyHandler.Properties.PropertyChanged += OnPropertyChanged;
+            {
+                //Subscribe to the requester's property list
+                systemSubscriptionRequestData.targetPropertyContainer.Properties.PropertyChanged += OnPropertyChanged;
+
+                //Initialise currently valid system choices
+                GiveInitialInteractions(systemSubscriptionRequestData.targetPropertyContainer, systemSubscriptionRequestData.targetPropertyContainer.Properties);
+            }
         }
 
-        protected virtual void OnPropertyChanged(object sender, EntityProperty updatedProperty)
+        public void GiveInteraction(PropertyContainer target, IInvokableInteraction interaction)
         {
-
+            target.Interactions.Add(interaction);
         }
+
+        /// <summary> Provide logic for co-ordinating property changes with their relevant methods. </summary>
+        protected abstract void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedData);
+
+        /// <summary> Provide the interactions which currently apply upon loading. </summary>
+        protected abstract void GiveInitialInteractions(PropertyContainer sourceContainer, PropertyHandler sourceProperties);
     }
 }
