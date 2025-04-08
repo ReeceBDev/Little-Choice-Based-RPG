83667c963c72cd1f6d3543946aa40ec11036578f
ReeceBDev <reece.bowen99@gmail.com>
2025-04-08 16:30:33 +0100

    New furniture type "Lightbulb"

---
+++ Little Choice-Based RPG/Resources/Entities/Physical/Furniture/Lightbulb.cs
@@ -0,0 +1,50 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Furniture
+{
+    internal class Lightbulb : InteractableObject
+    {
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            { "Component.BreakSystem", true },
+        };
+
+        static Lightbulb()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        public Lightbulb(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+    }
+}
