d921b1c42dd5365650eb6e8a87262ea18990de32
ReeceBDev <reece.bowen99@gmail.com>
2025-04-08 16:30:22 +0100

    Renamed property on DamageCommon from IsBroken to Damage.Broken, for clarity about its origin.

--- Little Choice-Based RPG/Resources/Systems/Damage/DamageCommon.cs
+++ Little Choice-Based RPG/Resources/Systems/Damage/DamageCommon.cs
@@ -12,7 +12,7 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage
         private static readonly DamageCommon singletonInstance = new DamageCommon();
         private DamageCommon()
         {
-            PropertyValidation.CreateValidProperty("IsBroken", PropertyType.Boolean); //Indicates when an object becomes broken. Used by BreakSystem and RepairSystem.
+            PropertyValidation.CreateValidProperty("Damage.Broken", PropertyType.Boolean); //Indicates when an object becomes broken. Used by BreakSystem and RepairSystem.
         }
 
         public static DamageCommon Instance
