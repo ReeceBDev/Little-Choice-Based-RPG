5dab1191fc634a4be62e22b3bcd2173221233906
ReeceBDev <reece.bowen99@gmail.com>
2025-04-08 16:38:12 +0100

    BreakSystem now implements its functionality correctly. This is akin to RepairSystem, but simplified, since BreakSystem only has one possible method at the moment.

--- Little Choice-Based RPG/Resources/Systems/Damage/Break/BreakSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/Damage/Break/BreakSystem.cs
@@ -3,72 +3,111 @@ using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
 using Little_Choice_Based_RPG.Resources.Systems.Damage.Repair;
 using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types.EntityProperties;
+using Little_Choice_Based_RPG.Types.Interactions.InteractDelegate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using static Little_Choice_Based_RPG.Types.Interactions.InteractDelegate.InteractionUsingNothing;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Break
 {
     public class BreakSystem : PropertyLogic
     {
-        private GameObject parentObject;
-
         static BreakSystem()
         {
             //BreakSystem logic
-            PropertyValidation.CreateValidProperty("IsBreakable", PropertyType.Boolean); //Activates all this class and all of these properties when true :)
-            PropertyValidation.CreateValidProperty("IsBreakableByChoice", PropertyType.Boolean); //Lets players choose to break it by choice. 
+            PropertyValidation.CreateValidProperty("Breakable.ByChoice", PropertyType.Boolean); //Lets players choose to break it by choice. 
 
             //Descriptors
-            PropertyValidation.CreateValidProperty("Descriptor.Breaking", PropertyType.String); //Describes how it looks when it is breaking.
-            PropertyValidation.CreateValidProperty("Descriptor.Choice.Break.Interact", PropertyType.String); //Describes the interact option presented to the player.
-            PropertyValidation.CreateValidProperty("Descriptor.Choice.Breaking", PropertyType.String); //Describes the action of breaking it when a player uses the Break() choice.
+            PropertyValidation.CreateValidProperty("Descriptor.Breakable.Interaction.Title", PropertyType.String); //Describes the interact option presented to the player.
+            PropertyValidation.CreateValidProperty("Descriptor.Breakable.Interaction.Invoking", PropertyType.String); //Describes the action of breaking it when a player uses the Break() choice.
             PropertyValidation.CreateValidProperty("Descriptor.Generic.Broken", PropertyType.String); //Broken at a distance
             PropertyValidation.CreateValidProperty("Descriptor.Inspect.Broken", PropertyType.String); //A closer look
         }
 
         /// <summary> Allows objects to break. Requires DamageCommon. </summary>
-        public BreakSystem(GameObject instantiatingObject, SystemSubscriptionEventBus systemSubscriptionEventBusReference) : base(systemSubscriptionEventBusReference)
+        public BreakSystem()
         {
-            /*
-            //Enforces IRepairable on the instantiating class
-            if (!(instantiatingObject is IBreakable))
-                throw new Exception($"{instantiatingObject.GetType()} does not implement IBreakable!");
-            */
-
             DamageCommon damageCommonInstantiation = DamageCommon.Instance;
-
-            //Stores instantiating object for accessing its PropertyHandler.
-            parentObject = instantiatingObject;
         }
 
-        /// <summary> Sets IsBroken to true. </summary>
-        public void Break(IUserInterface mutexHolder)
+        protected override void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedData)
+        {
+            PropertyContainer sourceContainer = propertyChangedData.sourceContainer;
+            PropertyHandler sourceProperties = sourceContainer.Properties;
+            EntityProperty newProperty = propertyChangedData.newProperty;
+
+            // When to do with Damage.Broken
+            if (newProperty.PropertyName == "Damage.Broken")
+            {
+                // When something breakable is repaired
+                if (newProperty.PropertyName == "Damage.Broken" && newProperty.PropertyValue.Equals(false))
+                {
+                    if (sourceProperties.HasProperty("Breakable.ByChoice", true))
+                        InitialiseBreakChoices(sourceContainer, sourceProperties);
+                }
+            }
+        }
+
+        protected override void GiveInitialInteractions(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        {
+            // If the object is not broken
+            if (sourceProperties.HasProperty("Damage.Broken", false))
+            {
+                if (sourceProperties.HasProperty("Breakable.ByChoice", true))
+                    InitialiseBreakChoices(sourceContainer, sourceProperties);
+            }
+        }
+
+
+        private void InitialiseBreakChoices(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        {
+            //Guard clauses for interaction descriptors
+            if (!sourceProperties.HasExistingPropertyName("Descriptor.Breakable.Interaction.Title"))
+                throw new Exception($"{sourceContainer} of ID {sourceProperties.GetPropertyValue("ID")} has no property of Descriptor.Breakable.Interaction.Title!");
+
+            if (!sourceProperties.HasExistingPropertyName("Descriptor.Breakable.Interaction.Invoking"))
+                throw new Exception($"{sourceContainer} of ID {sourceProperties.GetPropertyValue("ID")} has no property of Descriptor.Breakable.Interaction.Invoking!");
+
+
+            //Initialise the Interaction for Break()
+            InteractionUsingNothingDelegate breakUsingNothingDelegate = Break;
+
+            InteractionUsingNothing breakUsingNothing = new InteractionUsingNothing(
+                breakUsingNothingDelegate,
+                sourceContainer,
+                (string)sourceProperties.GetPropertyValue("Descriptor.Breakable.Interaction.Title"),
+                (string)sourceProperties.GetPropertyValue("Descriptor.Breakable.Interaction.Invoking")
+            );
+
+            GiveInteraction(sourceContainer, breakUsingNothing);
+        }
+
+
+        /// <summary> Sets Damage.Broken to true. </summary>
+        public void Break(IUserInterface mutexHolder, PropertyContainer sourceContainer)
         {
             //Guard clauses for the values in use.
-            if (!parentObject.Properties.HasProperty("IsBreakable", true))
-                throw new Exception("This object is not breakable! Tried to break an object where there is no EntityProperty of IsBreakable = true.");
+            if (sourceContainer.Properties.HasProperty("Damage.Broken", true))
+                throw new Exception("This object is already broken! Tried to break an object where there is already an EntityProperty of Damage.Broken = true.");
 
-            if (!parentObject.Properties.HasExistingPropertyName("Descriptor.Breaking"))
-                throw new Exception("This object has no break description! Tried to break an object where there is no EntityProperty of Descriptor.Breaking.");
-
-            if (!parentObject.Properties.HasExistingPropertyName("Descriptor.Generic.Broken"))
+            if (!sourceContainer.Properties.HasExistingPropertyName("Descriptor.Generic.Broken"))
                 throw new Exception("This object has no broken description! Tried to break an object where there is no EntityProperty of Descriptor.Generic.Broken.");
 
             //Main breaking logic.
-            parentObject.Properties.UpsertProperty("IsBroken", true); //Property found in DamageCommon
+            sourceContainer.Properties.UpsertProperty("Damage.Broken", true); //Property found in DamageCommon
 
             //Set generic descriptor.
-            parentObject.Properties.UpdateProperty("Descriptor.Generic.Current", parentObject.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
+            sourceContainer.Properties.ReplaceProperty("Descriptor.Generic.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
 
             //Set inspect descriptor or default to generic.
-            if (!parentObject.Properties.HasExistingPropertyName("Descriptor.Inspect.Broken"))
-                parentObject.Properties.UpdateProperty("Descriptor.Inspect.Current", parentObject.Properties.GetPropertyValue("Descriptor.Inspect.Broken"));
+            if (!sourceContainer.Properties.HasExistingPropertyName("Descriptor.Inspect.Broken"))
+                sourceContainer.Properties.ReplaceProperty("Descriptor.Inspect.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Inspect.Broken"));
             else
-                parentObject.Properties.UpdateProperty("Descriptor.Inspect.Current", parentObject.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
+                sourceContainer.Properties.ReplaceProperty("Descriptor.Inspect.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
         }
     }
 }
