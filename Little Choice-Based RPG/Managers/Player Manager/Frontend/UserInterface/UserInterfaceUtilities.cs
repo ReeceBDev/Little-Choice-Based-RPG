ed3a88d1bd4b365e8900676c9add524ad5f2854d
ReeceBDev <reece.bowen99@gmail.com>
2025-04-10 00:47:00 +0100

    Added new method to UserInterfaceUtilties named SplitIntoLines. SplitIntoLines will take a string and split it into several lines, complete with prefix, infix and suffix, up to a maximum character count per line. This enables dynamic styling when listing several pieces of text.

--- Little Choice-Based RPG/Managers/Player Manager/Frontend/UserInterface/UserInterfaceUtilities.cs
+++ Little Choice-Based RPG/Managers/Player Manager/Frontend/UserInterface/UserInterfaceUtilities.cs
@@ -50,5 +50,56 @@ namespace Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface
                 i--;
             }
         }
+
+        /// <summary> Splits a string into new lines based on a maximum line count, with a prefix, infix and suffix. </summary>
+        public static List<string> SplitIntoLines(string newEntry, string entryPrefix, 
+            string entryInfix, string entrySuffix, uint maximumLineLength = 120)
+        {
+            int prospectiveCharacterIndex = 0;
+            List<string> completedEntries = new List<string>();
+            string currentLine = entryPrefix;
+            string currentWord = "";
+
+            while (prospectiveCharacterIndex < newEntry.Length)
+            {
+                char prospectiveCharacter = newEntry[prospectiveCharacterIndex];
+
+                //if a whitespace does not follow a word
+                if (newEntry[prospectiveCharacterIndex].Equals(" ") && currentWord.Length == 0)
+                {
+                    prospectiveCharacterIndex++; //skip the whitespace
+                    continue;
+                }
+
+                //if (its a \n then handle it and maybe increase the new entry length by one because maybe this cuases the error?)
+                if (newEntry[prospectiveCharacterIndex].Equals('\n'))
+                {
+                    completedEntries.Add(currentLine);
+                    currentLine = entryInfix;
+                    prospectiveCharacterIndex++; //skip the new line character
+                    continue;
+                }
+
+                //While the current word contains at max only one whitespace (after itself)
+                while ((!currentWord.Contains(" ")) && prospectiveCharacterIndex < newEntry.Length)
+                    currentWord += newEntry[prospectiveCharacterIndex++];
+
+                //If the new total is too long, make a new line
+                if ((currentWord.Length + currentLine.Length) > maximumLineLength)
+                {
+                    completedEntries.Add(currentLine); //Save the current line
+
+                    //If the remaining characters fit on one line, add the suffix instead of the infix
+                    currentLine = (newEntry.Length - prospectiveCharacterIndex) < maximumLineLength ? entrySuffix : entryInfix; //Reset the line
+                }
+
+                currentLine += currentWord;
+                currentWord = ""; //Reset the currentWord
+            }
+
+            completedEntries.Add(currentLine); // Add the final line
+
+            return completedEntries;
+        }
     }
 }
