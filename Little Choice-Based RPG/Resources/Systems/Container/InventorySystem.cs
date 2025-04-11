c263f8e38116b47e5f7a2b07dff1fee6f2718a08
ReeceBDev <reece.bowen99@gmail.com>
2025-04-11 22:42:09 +0100

    Created Initial class for InventorySystem, which will be responsible for giving ProperyContainers the ability to store items and expose choices for adding and removing them. This will also be used as the basis for player inventories.

---
+++ Little Choice-Based RPG/Resources/Systems/Container/InventorySystem.cs
@@ -0,0 +1,18 @@
+﻿using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.Container
+{
+    internal class InventorySystem : PropertyLogic
+    {
+        static InventorySystem()
+        {
+            PropertyValidation.CreateValidProperty("Inventory.ID", uint);
+            PropertyValidation.CreateValidProperty("Inventory.Stored", uint);
+        }
+    }
+}
