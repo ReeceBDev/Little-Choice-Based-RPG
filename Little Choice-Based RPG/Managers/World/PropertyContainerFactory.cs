b273e73a0a3b4b471bb5883760fe48acc807f51b
ReeceBDev <reece.bowen99@gmail.com>
2025-04-15 16:11:21 +0100

    Changed the arguments on GameObjectFactory to produce PropertyContainers instead, and renamed accordingly.

--- Little Choice-Based RPG/Managers/World/PropertyContainerFactory.cs
+++ Little Choice-Based RPG/Managers/World/PropertyContainerFactory.cs
@@ -14,30 +14,30 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Managers.World
 {
-    /// <summary> Manufactures GameObjects of any type based on its properties. Gets allocated an outline of each object from JSONs. </summary>
-    public static class GameObjectFactory
+    /// <summary> Manufactures PropertyContainers of any type based on its properties. Gets allocated an outline of each object from JSONs. </summary>
+    public static class PropertyContainerFactory
     {
-        public static GameObject NewGameObject(Dictionary<string, object> properties)
+        public static PropertyContainer NewGameObject(Dictionary<string, object> properties)
         {
-            GameObject newGameObject = GenerateObjectFromTypeProperty(properties);
+            PropertyContainer newGameObject = GenerateObjectFromTypeProperty(properties);
             SubscribeToComponents(newGameObject, properties);
 
             return newGameObject;
         }
 
-        private static GameObject GenerateObjectFromTypeProperty(Dictionary<string, object> properties)
+        private static PropertyContainer GenerateObjectFromTypeProperty(Dictionary<string, object> properties)
         {
             if (!properties.ContainsKey("Type"))
                 throw new ArgumentException("The properties of the new object did not include the Type property! Properties: {properties}");
 
             Type objectType = Type.GetType(properties["Type"].ToString());
             object newGameObject = Activator.CreateInstance(objectType, properties);
-            GameObject castedGameObject = (GameObject)newGameObject;
+            PropertyContainer castedGameObject = (PropertyContainer)newGameObject;
 
             return castedGameObject;
         }
 
-        private static void SubscribeToComponents(GameObject targetGameObject, Dictionary<string, object> properties)
+        private static void SubscribeToComponents(PropertyContainer targetGameObject, Dictionary<string, object> properties)
         {
             foreach (EntityProperty property in targetGameObject.Properties.EntityProperties)
             {
