57f8fe92a2bf37ed25d0623c249f8f19bf905ec3
ReeceBDev <reece.bowen99@gmail.com>
2025-04-15 15:33:44 +0100

    New IExtension, provides an interface for the new Extension type, which provides new types of data for propertycontainers outside of properties. This is useful when needing lots of the same property, such as a contents list.

---
+++ Little Choice-Based RPG/Types/PropertyExtensions/IExtension.cs
@@ -0,0 +1,14 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.Extensions
+{
+    public interface IExtension
+    {
+        public string UniqueIdentifier { get; init; }
+    }
+}
