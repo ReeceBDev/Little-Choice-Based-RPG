470461c7623b25616a14e303e75a58ed26cd90de
ReeceBDev <reece.bowen99@gmail.com>
2025-04-15 15:38:26 +0100

    Renamed Damage namespace to DamageSystem to prevent conflicts

--- Little Choice-Based RPG/Resources/Systems/DamageSystems/Repair/RepairSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/DamageSystems/Repair/RepairSystem.cs
@@ -1,6 +1,6 @@
 ﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
-using Little_Choice_Based_RPG.Resources.Systems.Damage.Repair;
+using Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Repair;
 using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types.EntityProperties;
 using Little_Choice_Based_RPG.Types.Interactions.InteractDelegate;
@@ -12,10 +12,14 @@ using System.Threading.Tasks;
 using static Little_Choice_Based_RPG.Types.Interactions.InteractDelegate.InteractionUsingGameObject;
 using static Little_Choice_Based_RPG.Types.Interactions.InteractDelegate.InteractionUsingNothing;
 
-namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
+namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Repair
 {
     public class RepairSystem : PropertyLogic
     {
+        //This class requires DamageLogic.
+        DamageLogic damageCommonInstantiation = DamageLogic.Instance;
+
+        /// <summary> Allows repairs to occur on this object. Requires DamageLogic. </summary>
         static RepairSystem()
         {
             //RepairSystem logic
@@ -28,12 +32,6 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
             PropertyValidation.CreateValidProperty("Descriptor.Repair.Repairing", PropertyType.String); //Describes how it looks when it gets repaired.
             PropertyValidation.CreateValidProperty("Descriptor.Repair.Interaction.Title", PropertyType.String); //Describes the interact option presented to the player.
             PropertyValidation.CreateValidProperty("Descriptor.Repair.Interaction.Invoking", PropertyType.String); //Describes the action of repairing it when a player uses the Repair() choice.
-        }
-
-        /// <summary> Allows repairs to occur on this object. Requires DamageCommon. </summary>
-        public RepairSystem()
-        {
-            DamageCommon damageCommonInstantiation = DamageCommon.Instance;
         }
        
         protected override void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedData)
@@ -65,7 +63,7 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
         }
             
 
-        private void InitialiseRepairChoices(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        private static void InitialiseRepairChoices(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
             //Guard clauses for interaction descriptors
             if (!sourceProperties.HasExistingPropertyName("Descriptor.Repair.Interaction.Title"))
@@ -84,7 +82,7 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
                 GiveInteraction(sourceContainer, InitialiseRepairUsingTool(sourceContainer, sourceProperties));
         }
 
-        public InteractionUsingNothing InitialiseRepairUsingNothing(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        public static InteractionUsingNothing InitialiseRepairUsingNothing(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
             InteractionUsingNothingDelegate repairUsingNothingDelegate = InvokeRepair;
 
@@ -98,7 +96,7 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
             return repairUsingNothing;
         }
 
-        public InteractionUsingGameObject InitialiseRepairUsingTool(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        public static InteractionUsingGameObject InitialiseRepairUsingTool(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
             InteractionUsingGameObjectDelegate repairUsingGameObjectDelegate = InvokeRepair;
 
@@ -116,10 +114,10 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
         }
 
         /// <summary> Sets Damage.Broken to false, from the BreakSystem. </summary>
-        public void InvokeRepair(IUserInterface mutexHolder, PropertyContainer sourceContainer) => InvokeRepair(mutexHolder, sourceContainer, null);
+        public static void InvokeRepair(IUserInterface mutexHolder, PropertyContainer sourceContainer) => InvokeRepair(mutexHolder, sourceContainer, null);
 
         /// <summary> Sets Damage.Broken to false, from the BreakSystem. </summary>
-        public void InvokeRepair(IUserInterface mutexHolder, PropertyContainer sourceContainer, GameObject? repairTool = null)
+        public static void InvokeRepair(IUserInterface mutexHolder, PropertyContainer sourceContainer, GameObject? repairTool = null)
         {
             //Guard clauses for the values in use.
             if (!sourceContainer.Properties.HasProperty("Damage.Broken", true))
@@ -145,10 +143,10 @@ namespace Little_Choice_Based_RPG.Resources.Systems.Damage.Repair
             Repair(sourceContainer);
         }
 
-        private void Repair(PropertyContainer sourceContainer)
+        private static void Repair(PropertyContainer sourceContainer)
         {
             //Main repairing logic
-            sourceContainer.Properties.UpsertProperty("Damage.Broken", false); //Property found in DamageCommon
+            sourceContainer.Properties.UpsertProperty("Damage.Broken", false); //Property found in DamageLogic
 
             //Set the generic and inspect descriptors back to default.
             sourceContainer.Properties.ReplaceProperty("Descriptor.Generic.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Generic.Default"));
