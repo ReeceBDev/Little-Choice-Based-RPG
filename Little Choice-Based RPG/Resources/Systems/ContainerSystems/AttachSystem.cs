6d38f685ca98df8362660114fc5af11ed57a5be5
ReeceBDev <reece.bowen99@gmail.com>
2025-04-15 16:12:34 +0100

    Created AttachSystem which will be responsible for providing logic for attaching objects to eachother physically, so that they can move together, such as tying two planks together with rope.

---
+++ Little Choice-Based RPG/Resources/Systems/ContainerSystems/AttachSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.ContainerSystems
+{
+    /// <summary> Allows objects to connect to other objects within their room/container. Will follow them around, since they are attached. Requires WeightbearingLogic</summary>
+    public class AttachSystem : PropertyLogic
+    {
+        WeightbearingLogic weightBearingLogicInstantiation = WeightbearingLogic.Instance;
+    }
+}
