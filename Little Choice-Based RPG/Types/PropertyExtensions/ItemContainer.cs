180b17cca7a1cbb18c3f7c6a1b9eba8c70081755
ReeceBDev <reece.bowen99@gmail.com>
2025-04-15 15:33:01 +0100

    New ItemContainer, which holds direct references to GameObject on an entity. Uses IExtension

---
+++ Little Choice-Based RPG/Types/PropertyExtensions/ItemContainer.cs
@@ -0,0 +1,19 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.Extensions
+{
+    public class ItemContainer : IExtension
+    {
+        public void Remove(GameObject target) => Inventory.Remove(target);
+        public void Add(GameObject target) => Inventory.Add(target);
+
+        public string UniqueIdentifier { get; init; } = "Inventory";
+        public List<GameObject> Inventory { get; private set; } = new List<GameObject>();
+    }
+}
