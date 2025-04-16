d875ef495b618447896c766aad5c519c4c76d94b
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:38:15 +0100

    Created new PrivateInteractions class which lists Interactions which should only be accessed by a player themselves. This could be lockable, later, for enforcement.

---
+++ Little Choice-Based RPG/Types/PropertyExtensions/Extensions/PrivateInteractions.cs
@@ -0,0 +1,19 @@
+﻿using Little_Choice_Based_RPG.Types.Interactions.InteractDelegate;
+using Little_Choice_Based_RPG.Types.PropertyExtensions.ExtensionEventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.PropertyExtensions.Extensions
+{
+    public class PrivateInteractions : IExtension
+    {
+        public List<IInvokableInteraction> PrivateInteractionsList { get; set; }
+
+        public event EventHandler<ExtensionChangedArgs> ExtensionChanged; //No need to invoke this yet, it has no practical purpose (yet).
+        public string UniqueIdentifier { get; init; } = "PrivateInteractions";
+
+    }
+}
