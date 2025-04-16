382dfec03604d096846ced0f7deedf4891d7b8dc
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:35:54 +0100

    Defined an ExtensionChanged event in ItemContainer to notify when an extension has been modified.
    Extensions must define their own changed event details. This complies with the same format as the PropertyChanged event under PropertyHandler, which lets objects unify their *Changed events.
    
    ItemContainer defines two ExtensionChanged events, ItemContainer.Added and ItemContainer.Removed.
    
    Moved ItemContainer and IExtension into the Extensions sub-folder under Types/PropertyExtensions.

--- Little Choice-Based RPG/Types/PropertyExtensions/Extensions/IExtension.cs
+++ Little Choice-Based RPG/Types/PropertyExtensions/Extensions/IExtension.cs
@@ -1,14 +1,16 @@
 ﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.PropertyExtensions.ExtensionEventArgs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Little_Choice_Based_RPG.Types.Extensions
+namespace Little_Choice_Based_RPG.Types.PropertyExtensions.Extensions
 {
     public interface IExtension
     {
         public string UniqueIdentifier { get; init; }
+        public event EventHandler<ExtensionChangedArgs> ExtensionChanged;
     }
 }
---
+++ Little Choice-Based RPG/Types/PropertyExtensions/Extensions/ItemContainer.cs
@@ -0,0 +1,30 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using Little_Choice_Based_RPG.Types.PropertyExtensions.ExtensionEventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.PropertyExtensions.Extensions
+{
+    public class ItemContainer : IExtension
+    {
+        public void Add(GameObject target)
+        {
+            ExtensionChanged?.Invoke(this, new ExtensionChangedArgs("ItemContainer.Added", target));
+            Inventory.Add(target);
+        }
+
+        public void Remove(GameObject target)
+        {
+            ExtensionChanged?.Invoke(this, new ExtensionChangedArgs("ItemContainer.Removed", target));
+            Inventory.Remove(target);
+        }
+
+        public event EventHandler<ExtensionChangedArgs> ExtensionChanged;
+        public string UniqueIdentifier { get; init; } = "ItemContainer";
+        public List<GameObject> Inventory { get; private set; } = new List<GameObject>();
+    }
+}
