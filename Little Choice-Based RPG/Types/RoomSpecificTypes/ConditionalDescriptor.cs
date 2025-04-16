589b4650850d452c8240ea18faeb8ab932fb739f
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:32:37 +0100

    Moved ConditionalDescriptors into its own class file

---
+++ Little Choice-Based RPG/Types/RoomSpecificTypes/ConditionalDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.RoomTypes
+{
+    public record struct ConditionalDescriptor(string Descriptor, List<EntityState> RequiredEntityStates, uint Priority = 6);
+}
