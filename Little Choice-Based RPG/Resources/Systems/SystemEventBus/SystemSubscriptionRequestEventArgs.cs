cde2f3a87fd2e2362a45acc78a8c78566a5c5578
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:56:04 +0100

    Moved SystemSubscriptionRequestEventArgs into its own class.

---
+++ Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionRequestEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.SystemEventBus
+{
+    public record struct SystemSubscriptionRequestEventArgs(PropertyContainer targetPropertyContainer, string systemReferenceName);
+}
