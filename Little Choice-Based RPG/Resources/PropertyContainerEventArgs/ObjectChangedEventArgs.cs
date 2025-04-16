222f7ac170756287f33de12c55434201ac934520
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:46:52 +0100

    Created new record struct ObjectChangedEventArgs

---
+++ Little Choice-Based RPG/Resources/PropertyContainerEventArgs/ObjectChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.EventArgs
+{
+    public record struct ObjectChangedEventArgs(PropertyContainer SourceContainer, string IdentifierChanged, object ValueChanged);
+}
