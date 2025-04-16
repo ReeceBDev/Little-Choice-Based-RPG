04e6d23e5123d1db5335c6a7794fbb21fbf1c495
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:31:50 +0100

    Moved RoomType enum into its own class file

---
+++ Little Choice-Based RPG/Types/RoomSpecificTypes/RoomType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.RoomTypes
+{
+    public enum RoomType
+    {
+        Desert,
+        Town
+    }
+}
