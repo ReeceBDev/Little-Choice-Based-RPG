85cd62e060ce858b318eb7bb240e18a276fa9d1a
ReeceBDev <reece.bowen99@gmail.com>
2025-04-16 23:31:35 +0100

    Moved player specific logic out of textbasedRPG into its own class, PlayerController. Now each player is spawned by creating a new PlayerController. The playercontroller handles the rest itself

--- Little Choice-Based RPG/TextBasedRPG.cs
+++ Little Choice-Based RPG/TextBasedRPG.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Xml;
+using Little_Choice_Based_RPG.Managers.Player_Manager;
 using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Managers.World;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
@@ -34,46 +35,12 @@ internal class TextBasedRPG
         new BreakSystem().Instance;
         new GearSystem().Instance;
 
-        //Main
-        var mainWorld = new GameEnvironment();
-        mainWorld.GenerateAllRooms();
+        //Generate the environment
+        GameEnvironment currentEnvironment = new GameEnvironment();
+        currentEnvironment.GenerateAllRooms(); //Generate rooms within the environment
 
-        uint spawnRoomID = mainWorld.Rooms.ElementAt(1).Key;
-
-        //Set player spawn position
-        Dictionary<string, object> playerProperties = new Dictionary<string, object>();
-        playerProperties.Add("Position", spawnRoomID);
-
-        //Create a helmet
-        Dictionary<string, object> davodianMk1Helmet = new Dictionary<string, object>();
-        davodianMk1Helmet.Add("Name", "Davodian MkI Covered Faceplate");
-        davodianMk1Helmet.Add("Type", "Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour.Helmet");
-        davodianMk1Helmet.Add("Descriptor.Generic.Default", "Discarded aside here is your original Davodian MkI, its dented gunmetal grey faceplate long lustreless.");
-        davodianMk1Helmet.Add("Descriptor.Inspect.Default", "Disfigured and mottled from years of abuse, the gunmetal grey protective sensors relay a live feed, protecting your vision and perceptible hearing.\r\nThe units bodywork has been long since reworked in patchwork copper shielding - under the seams near-charred components made up of scrapped debris poke through residual sand.");
-        davodianMk1Helmet.Add("Descriptor.Equip", "Dusty residue coats your hands as you heave the thick, heavy metal above you and press your forehead in.\r\nInterlocking clicks engage behind your neck, a gentle buzz as the metal comes online.");
-        davodianMk1Helmet.Add("Descriptor.Unequip", "Engaging the clasp at the rear, the locks reluctantly scrape their disengaging clicks and the full weight of the visor bears down on your head.\r\nSandpaper lining scratches the sides of your face when you tilt your head forwards and force the faceplate off.");
-
-        davodianMk1Helmet.Add("Component.RepairSystem", true);
-        davodianMk1Helmet.Add("Repairable.ByChoice", true);
-        davodianMk1Helmet.Add("Descriptor.Repair.Interaction.Title", "Repair - Re - calibrate the helmets longitudinal wave sensor - array.");
-        davodianMk1Helmet.Add("Descriptor.Repair.Interaction.Invoking", "You fixed the helmet, yayy!");
-
-        davodianMk1Helmet.Add("Component.BreakSystem", true);
-        davodianMk1Helmet.Add("Breakable.ByChoice", true);
-        davodianMk1Helmet.Add("Descriptor.Breakable.Interaction.Title", "Damage - Intentionally misalign the helmets longitudinal wave sensor-array");
-        davodianMk1Helmet.Add("Descriptor.Breakable.Interaction.Invoking", "You broke the helmet oh nooo!");
-        GameObject testDavodian = PropertyContainerFactory.NewGameObject(davodianMk1Helmet);
-
-        //Give the player a helmet
-        playerProperties.Add("Gear.Slot.Helmet.ID", testDavodian.Properties.GetPropertyValue("ID"));
-
-        //Main cont.
-        var currentPlayer = new Player(playerProperties);
-        var currentUserInterfaceHandler = new UserInterfaceHandler(currentPlayer, mainWorld);
-
-        while (true)
-        {
-            currentUserInterfaceHandler.GenerateOutput();
-        }
+        //Generate a player
+        Room spawnRoom = currentEnvironment.Rooms.ElementAt(1).Value; //Select the room to spawn the player in
+        PlayerController player1 = new PlayerController(spawnRoom, currentEnvironment); //Create the player in that room
     }
 }
