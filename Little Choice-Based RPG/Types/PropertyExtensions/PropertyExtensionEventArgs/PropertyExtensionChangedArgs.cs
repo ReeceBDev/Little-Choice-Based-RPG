4bc3b1400eae87bebad6a6387a38bd6fb0004d56
ReeceBDev <reece.bowen99@gmail.com>
2025-04-17 19:13:18 +0100

    Renamed PropertyExtensionChangedArgs namespace from ExtensionEventArgs to PropertyExtensionEventArgs

--- Little Choice-Based RPG/Types/PropertyExtensions/PropertyExtensionEventArgs/PropertyExtensionChangedArgs.cs
+++ Little Choice-Based RPG/Types/PropertyExtensions/PropertyExtensionEventArgs/PropertyExtensionChangedArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Little_Choice_Based_RPG.Types.PropertyExtensions.ExtensionEventArgs
+namespace Little_Choice_Based_RPG.Types.PropertyExtensions.PropertyExtensionEventArgs
 {
     public record struct PropertyExtensionChangedArgs(string UniqueIdentifier, object SubjectChanged);
 }
