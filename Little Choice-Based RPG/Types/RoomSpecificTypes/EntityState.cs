7879e6003ee073bdd58688b55efd43a3f324aea2
ReeceBDev <reece.bowen99@gmail.com>
2025-04-17 19:12:05 +0100

    Renamed EntityState namespace from RoomTypes to RoomSpecificTypes

--- Little Choice-Based RPG/Types/RoomSpecificTypes/EntityState.cs
+++ Little Choice-Based RPG/Types/RoomSpecificTypes/EntityState.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Little_Choice_Based_RPG.Types.RoomTypes
+namespace Little_Choice_Based_RPG.Types.RoomSpecificTypes
 {
     /// <summary> Note: An ID without an EntityProperty should just be checked for being present </summary>
     public record struct EntityState(uint EntityReferenceID, List<EntityProperty>? RequiredProperties);
