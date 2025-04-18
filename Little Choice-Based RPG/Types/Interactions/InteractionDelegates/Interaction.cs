35f6557ffc452add910ba8c99e9f71b03485398b
ReeceBDev <reece.bowen99@gmail.com>
2025-04-19 00:47:20 +0100

    Added Equality overrides for Interaction. This allows interactions to be equated by specific implementation or by the method they are calling.
    Created a DelegateRecord which caches the delegate assigned in each derived constructor.
    
    The new DelegateRecord is used alongside the derived Type, the invocationMutexIdentity and the AssociatedSource for checking equality.
    
    Created overrides for Equals, GetHashCode, == operator and != operator.

--- Little Choice-Based RPG/Types/Interactions/InteractionDelegates/Interaction.cs
+++ Little Choice-Based RPG/Types/Interactions/InteractionDelegates/Interaction.cs
@@ -24,9 +24,11 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates
         }
 
         /// <summary> Creates a new interaction for players to be presented with in ChoiceHandler. </summary>
-        public Interaction(PropertyContainer setSourceContainer, string setInteractTitle, string setInteractDescriptor, InteractionRole setInteractRole = InteractionRole.Explore)
+        public Interaction(Delegate setDelegateRecord, PropertyContainer setSourceContainer, string setInteractTitle, string setInteractDescriptor, InteractionRole setInteractRole = InteractionRole.Explore)
         {
-            SourceContainer = setSourceContainer;
+            DelegateRecord = setDelegateRecord; // Mirrors the delegate set in the derived class.
+
+            AssociatedSource = setSourceContainer;
             InteractionContext = setInteractRole;
             InteractionTitle = setInteractTitle;
             InteractDescriptor = setInteractDescriptor;
@@ -50,8 +52,68 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates
         public abstract void GiveRequiredParameter(object newParameter, PlayerController sourceInvocationMutexIdentity);
         protected abstract void Invoke(PlayerController sourceInvocationMutexIdentity);
 
-        /// <summary> The originating PropertyContainer </summary>
-        public PropertyContainer SourceContainer { get; init; }
+        public static bool operator !=(Interaction firstTarget, Interaction secondTarget) => !(firstTarget == secondTarget);
+        public static bool operator ==(Interaction firstTarget, Interaction secondTarget)
+        {
+            if (firstTarget is null && secondTarget is null)
+                return true;
+
+            if (firstTarget is null && secondTarget is not null)
+                return false;
+
+            if (firstTarget is not null && secondTarget is null)
+                return false;
+
+            return firstTarget.Equals(secondTarget);
+        }
+        public override int GetHashCode() 
+            => (this.GetType(), invocationMutexIdentity.CurrentPlayer.Properties.GetPropertyValue("ID"), 
+            AssociatedSource.Properties.GetPropertyValue("ID"), DelegateRecord).GetHashCode();
+        public override bool Equals(object target) => this.Equals(target as Interaction);
+        public bool Equals(Interaction target)
+        {
+            //Null equivalence
+            if (target is null)
+                return false;
+
+            //Reference equivalence
+            if (Object.ReferenceEquals(this, target))
+                return true; //Returns true early, for optimisation.
+
+            //Derived type equivalence
+            if (this.GetType() != target.GetType())
+                return false;
+
+            //Player identity equivalence
+            if (this.invocationMutexIdentity.CurrentPlayer.Properties.GetPropertyValue("ID") != target.invocationMutexIdentity.CurrentPlayer.Properties.GetPropertyValue("ID"))
+                return false;
+            
+            //Source equivalence
+            if (this.AssociatedSource.Properties.GetPropertyValue("ID") != target.AssociatedSource.Properties.GetPropertyValue("ID"))
+                return false;
+
+            //Delegate equivalence
+            if (this.DelegateRecord != target.DelegateRecord)
+                return false;
+
+            //Note:
+            //Explicitly avoid InteractionTitle and InteractionDescriptor checks as these values may change, yet the delegate should still be the same.
+            //(InteractionTitle and InteractionDescriptor are transient properties from the object that represent a snapshot in time)
+            //(Whereas this delegate is a way of delivering a specific interaction to a specific object in a specific way.)
+            //(And as such, that delivery is what should be compared, not the title or descriptor!)
+
+            return true;
+        }
+
+        /// <summary> 
+        /// The PropertyContainer that invoking is performed on, or whatever PropertyContainer the delegate relates to the most. 
+        /// If this delegate gets stored on a GameObject, then AssociatedSource is that GameObject. 
+        /// If this is stored privately on a player, it is instead the target object that it would have effected. 
+        /// This value may reference a player only if the player is directly effected by the system.
+        /// This value may reference a room only if it is specifically the room which is the main target of operation.
+        /// (Such as in en-masse operations that make sweeping changes to a room.)
+        /// </summary>
+        public PropertyContainer AssociatedSource { get; init; }
 
         /// <summary> The title shown when a player gets listed their choice options. </summary>
         public string InteractionTitle { get; init; }
@@ -61,5 +123,9 @@ namespace Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates
 
         /// <summary> Describes how an Interaction should be presented by the User Interface, for example, if it belongs to a context-menu. </summary>
         public InteractionRole InteractionContext { get; init; }
+
+        /// <summary> Represents the method being invoked, in the specific way designated by delegates' type. 
+        /// Only used for equivalence. Mirrors the delegate in the derived class. </summary>
+        public Delegate DelegateRecord { get; init; }
     }
 }
