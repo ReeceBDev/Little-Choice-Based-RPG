f645cc29d07290d0a81cf77f13a3b71b4454a047
ReeceBDev <reece.bowen99@gmail.com>
2025-04-20 23:56:10 +0100

    Added new (currently empty) class DescriptorSystem which inherits from PropertyLogic and will be responsible for initialising anything to do with DescriptorLogic for propertycontainers which subscribe to it. All rooms and all gameobjects should subscribe to this by default, since they will all contain descriptors.

---
+++ Little Choice-Based RPG/Resources/Systems/InformationalSystems/Descriptor/DescriptorSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.DescriptorSystems.Descriptor
+{
+    public class DescriptorSystem : PropertyLogic
+    {
+    }
+}
