f26ee1c2ea48cf0f1e341ae5bae9567b22b6d721
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 00:04:54 +0100

    Defined a new IDescriptorCondition interface to allow for multiple types of conditional descriptors to exist.

---
+++ Little Choice-Based RPG/Types/DescriptorConditions/IDescriptorCondition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.DescriptorConditions
+{
+    public interface IDescriptorCondition
+    {
+        public string GetDescriptor();
+        public ConditionType ConditionIdentity { get; init; }
+        public string DescriptorIdentity { get; init; }      
+        public uint Priority { get; init; }
+        public List<EntityState> RequiredEntityStates { get; init; }
+    }
+}
