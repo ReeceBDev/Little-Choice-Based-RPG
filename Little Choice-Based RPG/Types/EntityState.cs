b7a5669187ed55247275496fd056c186b3c3f0e8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 00:02:22 +0100

    EntityState went directly into the Types namespace

---
+++ Little Choice-Based RPG/Types/EntityState.cs
@@ -0,0 +1,12 @@
+﻿using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types
+{
+    /// <summary> A possible state or existence of a GameObject. Note: An ID without an EntityProperty will just be checked for being present. </summary>
+    public record struct EntityState(uint EntityReferenceID, List<EntityProperty>? RequiredProperties);
+}
