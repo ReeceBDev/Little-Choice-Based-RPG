990151e12ea03ebecee37002d234a2c20e99e58f
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 00:03:14 +0100

    New ConditionType class under DescriptorConditions defines possible enums for descriptor identities.

---
+++ Little Choice-Based RPG/Types/DescriptorConditions/ConditionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.DescriptorConditions
+{
+    public enum ConditionType
+    {
+        RequireEntityStates,
+    }
+}
