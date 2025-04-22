a6efe36e90f7bf12af380fb8120c8b944dbd7513
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 01:21:37 +0100

    ConditionalDescriptors class updated to use IDescriptorCondition instead of ConditionalDescriptor, to allow for different types of conditions

--- Little Choice-Based RPG/Resources/Systems/InformationalSystems/Descriptor/DescriptorExtensions/ConditionalDescriptors.cs
+++ Little Choice-Based RPG/Resources/Systems/InformationalSystems/Descriptor/DescriptorExtensions/ConditionalDescriptors.cs
@@ -6,20 +6,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Little_Choice_Based_RPG.Types.RoomSpecificTypes;
+using Little_Choice_Based_RPG.Types.DescriptorConditions;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descriptor.DescriptorExtensions
 {
     /// <summary> Holds conditional descriptors. Each conditional descriptor is a possible descriptor that applies under a condition. </summary>
     public class ConditionalDescriptors : IPropertyExtension
     {
-        public void Add(ConditionalDescriptor condition)
+        public void Add(IDescriptorCondition condition)
         {
             PropertyExtensionChanged?.Invoke(this, new PropertyExtensionChangedArgs("ConditionalDescriptor.Added", condition));
             Descriptors.Add(condition);
         }
 
-        public void Remove(ConditionalDescriptor condition)
+        public void Remove(IDescriptorCondition condition)
         {
             PropertyExtensionChanged?.Invoke(this, new PropertyExtensionChangedArgs("ConditionalDescriptor.Removed", condition));
             Descriptors.Remove(condition);
@@ -27,6 +27,6 @@ namespace Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descrip
 
         public event EventHandler<PropertyExtensionChangedArgs> PropertyExtensionChanged;
         public string UniqueIdentifier { get; init; } = "ConditionalDescriptors";
-        public List<ConditionalDescriptor> Descriptors { get; private set; } = new List<ConditionalDescriptor>();
+        public List<IDescriptorCondition> Descriptors { get; private set; } = new List<IDescriptorCondition>();
     }
 }
