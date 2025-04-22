795ea9a8f5c1b478ea87adee329150a0b4bd3966
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 01:14:06 +0100

    The PlayerSystem is no longer responsible for holding an inventory system, listen to property changes or generate inventory choices. Instead, this logic is distributed to more sensible systems across the codespace. Since it was unimplemented, describing the new locations for this logic is not warranted, since nothing was actually replaced.

--- Little Choice-Based RPG/Resources/Systems/PlayerSystems/PlayerSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/PlayerSystems/PlayerSystem.cs
@@ -1,4 +1,4 @@
-﻿using Little_Choice_Based_RPG.Resources.Extensions;
+﻿using Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs;
 using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems;
 using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types.EntityProperties;
@@ -10,50 +10,27 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.PlayerSystems
 {
-    /// <summary> Implements player-specific logic. Requires InventoryLogic.</summary>
+    /// <summary> Implements player-specific logic.</summary>
     public class PlayerSystem : PropertyLogic
     {
-        //This class requires InventorySystem.
-        InventorySystem inventorySystem = InventorySystem.Instance;
-
         static PlayerSystem()
         {
             PropertyValidation.CreateValidProperty("Player.CanHear", PropertyType.Boolean);
             PropertyValidation.CreateValidProperty("Player.CanSee", PropertyType.Boolean);
         }
 
-        protected override void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedData)
-        {
-            if (propertyChangedData.newProperty.PropertyName == "Position")// player room changed
-                GenerateInventoryChoices(propertyChangedData.sourceContainer, propertyChangedData.sourceContainer.Properties); 
-        }
-
         protected override void InitialiseNewSubscriber(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
-            InitialiseInventoryComponent(sourceContainer, sourceProperties);
-            GenerateInventoryChoices(sourceContainer, sourceProperties);
-        }
-
-        private void InitialiseInventoryComponent(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
-        {
-            //The subscriber requires Component.InventorySystem. This then checks for the required inventory properties itself :)
-            if (!sourceProperties.HasExistingPropertyName("Component.InventorySystem"))
+            //The subscriber requires Component.PrivateInteractionsSystem.
+            if (!sourceProperties.HasExistingPropertyName("Component.PrivateInteractionsSystem"))
             {
-                sourceProperties.CreateProperty("Component.InventorySystem", true); //Creates the inventory component.
-                SystemSubscriptionEventBus.Subscribe(sourceContainer, "InventorySystem"); //Subscribes the source to the component.
+                sourceProperties.CreateProperty("Component.PrivateInteractionsSystem", true); //Creates the PrivateInteractions component.
+                SystemSubscriptionEventBus.Subscribe(sourceContainer, "PrivateInteractionsSystem"); //Subscribes the source to the component.
             }
         }
 
-        private void GenerateInventoryChoices(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        protected override void OnObjectChanged(object sender, ObjectChangedEventArgs objectChangedData)
         {
-            //Get room
-            //Iterate through each item in the room
-            //Give the player pick=-ups for them based on their capability
-            //Generate moves
-
-            //Iterate through player stored inventory items
-            //Generate put-downs
-            //Generate moves
         }
     }
 }
