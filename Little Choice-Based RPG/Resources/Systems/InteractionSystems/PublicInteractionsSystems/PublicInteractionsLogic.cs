30f46616bb7a5ed10bfe3d18e639623e3191f7c7
ReeceBDev <reece.bowen99@gmail.com>
2025-04-22 01:14:40 +0100

    New PublicInteractionsLogic class allows the codebase to use GiveInteraction, instead of that being on PropertyLogic.

---
+++ Little Choice-Based RPG/Resources/Systems/InteractionSystems/PublicInteractionsSystems/PublicInteractionsLogic.cs
@@ -0,0 +1,17 @@
+﻿using Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.InteractionSystems.PublicInteractionsSystems
+{
+    public static class PublicInteractionsLogic
+    {
+        public static void GiveInteraction(PropertyContainer target, IInvokableInteraction interaction)
+        {
+            target.Interactions.Add(interaction);
+        }
+    }
+}
