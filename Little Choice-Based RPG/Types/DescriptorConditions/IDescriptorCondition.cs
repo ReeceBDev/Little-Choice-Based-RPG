09bca519d8499af0f6eee14620e45a21d23eb1b2
ReeceBDev <reece.bowen99@gmail.com>
2025-04-23 23:02:48 +0100

    Added CheckConditionIsValid to the interface IDescriptorCondition

--- Little Choice-Based RPG/Types/DescriptorConditions/IDescriptorCondition.cs
+++ Little Choice-Based RPG/Types/DescriptorConditions/IDescriptorCondition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace Little_Choice_Based_RPG.Types.DescriptorConditions
     public interface IDescriptorCondition
     {
         public string GetDescriptor();
+        public bool CheckConditionIsValid(PropertyContainer targetEntity, IDescriptorCondition targetStateDescriptor);
         public ConditionType ConditionIdentity { get; init; }
         public string DescriptorIdentity { get; init; }      
         public uint Priority { get; init; }
