eb6ce187b41e36ad7e85a838208c47564184073c
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:18:35 +0100

    New InteractionRole SystemInventoryEntry which is used by the UI

--- Little Choice-Based RPG/Types/Interactions/InteractionRole.cs
+++ Little Choice-Based RPG/Types/Interactions/InteractionRole.cs
@@ -12,7 +12,10 @@ namespace Little_Choice_Based_RPG.Types.Interactions
         Explore, //Default. Should show most of the time, such as when exploring or when opening an inspect context-menu.
         Inspect, //Should be applied upon inspecting an object and opening its context-menu.
         Navigation, //Should be applied to choices relating to moving around, such as Direction.
-        System //Should apply to anything relating to the meta system, for example, exiting the game, navigating to the options menu, and so on.
+        System, //Should apply to anything relating to the meta system, for example, exiting the game, navigating to the options menu, and so on.
+
+        //Unique
+        SystemInventoryEntry //Only used by the inventory screen, to different inventory elements. Therefore, it doesn't need to be considered, ever.
     }
 
 }
