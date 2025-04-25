b7734cf3094ea33dec5d379944624e5ac10763b8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:16:32 +0100

    New WeightbearingSystem replaces the WeightbearingObject class and provides functionality for adding and removing weight up until a maximum strength capacity of any item container that has a strength value (which at the moment, is enforced to be anything except Room).
    This is required by InventorySystem and should always run first.

---
+++ Little Choice-Based RPG/Resources/Systems/ContainerSystems/Weightbearing/WeightbearingSystem.cs
@@ -0,0 +1,42 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs;
+using Little_Choice_Based_RPG.Resources.Rooms;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Weightbearing
+{
+    /// <summary> Gives objects strength to hold weight. Decides if further objects may be carried. Requires something to use it, i.e. InventorySystem, GearSystem. All weight and strength is in KG (Kilogrammes). </summary>
+    public class WeightbearingSystem : PropertyLogic
+    {
+        static WeightbearingSystem()
+        {
+            PropertyValidation.CreateValidProperty("Weightbearing.WeightHeldInKG", PropertyType.Decimal); // Weight currently held
+            PropertyValidation.CreateValidProperty("Weightbearing.StrengthInKG", PropertyType.Decimal); //Maximum weight carriable
+        }
+        protected override void InitialiseNewSubscriber(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
+        {
+            //Require StrengthInKG, unless it's a Room.
+            if (!sourceProperties.GetPropertyValue("Type").Equals("Little_Choice_Based_RPG.Resources.Rooms.Room"))
+                if (!sourceProperties.HasExistingPropertyName("Weightbearing.StrengthInKG"))
+                    throw new Exception($"{sourceContainer} of ID {sourceProperties.GetPropertyValue("ID")} has no property of Weightbearing.StrengthInKG!");
+        }
+
+        protected override void OnObjectChanged(object sender, ObjectChangedEventArgs propertyChangedData)
+        {
+            //Add or remove weight, unless it's a Room.
+            if (propertyChangedData.Source is not Room)
+            {
+                if (propertyChangedData.Property.Equals("ItemContainer.Added"))
+                    WeightbearingProcessor.AddWeight((GameObject)propertyChangedData.Source, (GameObject)propertyChangedData.Change);
+
+                if (propertyChangedData.Property.Equals("ItemContainer.Removed"))
+                    WeightbearingProcessor.DropWeight((GameObject)propertyChangedData.Source, (GameObject)propertyChangedData.Change);
+            }
+        }
+    }
+}
