e3bad806676b3443cc39d165f7b1359ad92e35b9
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:22:10 +0100

    lobalDamageProperties replaces the former DamageLogic, which is a far better name.

---
+++ Little Choice-Based RPG/Resources/Systems/DamageSystems/GlobalDamageProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems
+{
+    public static class GlobalDamageProperties
+    {
+        static GlobalDamageProperties()
+        {
+            PropertyValidation.CreateValidProperty("Damage.Broken", PropertyType.Boolean); //Indicates when an object becomes broken. Used by BreakSystem and RepairSystem.
+        }
+    }
+}
