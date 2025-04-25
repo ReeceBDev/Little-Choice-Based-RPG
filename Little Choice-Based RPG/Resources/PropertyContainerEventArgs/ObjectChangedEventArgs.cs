d23dfcc8a575116b57485ff707d2e71035c9a6b6
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:24:23 +0100

    Renamed elements in ObjectChangedEventArgs to be more sensible for my mind to understand when using them.

--- Little Choice-Based RPG/Resources/PropertyContainerEventArgs/ObjectChangedEventArgs.cs
+++ Little Choice-Based RPG/Resources/PropertyContainerEventArgs/ObjectChangedEventArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Little_Choice_Based_RPG.Resources.EventArgs
+namespace Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs
 {
-    public record struct ObjectChangedEventArgs(PropertyContainer SourceContainer, string IdentifierChanged, object ValueChanged);
+    public record struct ObjectChangedEventArgs(PropertyContainer Source, string Property, object Change);
 }
