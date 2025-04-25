4f49e48bc537813e9fd209646cbd00ae5f15d40e
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:10:45 +0100

    New ItemContainer extension holds a list of GameObject that can be placed onto a PropertyContainer.
    Each object subscribes to this and announces the addition and removal of objects from and into this list as if it were a property update.
    
    Because this ItemContainer will be replacing Room contents, too, two additional events have been added:
    - NewLocalUserMessage, which broadcasts a user message locally within the container
    - NewGlobalUserMessage, which broadcasts a user message globally, to all containers.

---
+++ Little Choice-Based RPG/Resources/Systems/ContainerSystems/Inventory/InventoryExtensions/ItemContainer.cs
@@ -0,0 +1,44 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using Little_Choice_Based_RPG.Types.PropertyExtensions;
+using Little_Choice_Based_RPG.Types.PropertyExtensions.PropertyExtensionEventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory.InventoryExtensions
+{
+    public class ItemContainer : IPropertyExtension
+    {
+        public void Add(GameObject target)
+        {
+            Inventory.Add(target);
+            PropertyExtensionChanged?.Invoke(this, new PropertyExtensionChangedArgs("ItemContainer.Added", target));
+        }
+
+        public void Remove(GameObject target)
+        {
+            Inventory.Remove(target);
+            PropertyExtensionChanged?.Invoke(this, new PropertyExtensionChangedArgs("ItemContainer.Removed", target));
+        }
+
+        public void NewLocalUserMessage(string userMessage)
+        {
+            BroadcastLocalUserMessage?.Invoke(null, userMessage + "§f"); //§f Resets any custom colours back to white at the end of the statement.
+        }
+
+        public static void NewGlobalUserMessage(string userMessage)
+        {
+            BroadcastGlobalUserMessage?.Invoke(null, userMessage + "§f"); //§f Resets any custom colours back to white at the end of the statement.
+        }
+
+        public static event EventHandler<string> BroadcastGlobalUserMessage; //Broadcasts a message to the players within any and all containers, yes, all.
+        public event EventHandler<string> BroadcastLocalUserMessage; //Broadcasts a message to the players within this container.
+        public event EventHandler<PropertyExtensionChangedArgs> PropertyExtensionChanged;
+        public string UniqueIdentifier { get; init; } = "ItemContainer";
+        public List<GameObject> Inventory { get; private set; } = new List<GameObject>();
+    }
+}
