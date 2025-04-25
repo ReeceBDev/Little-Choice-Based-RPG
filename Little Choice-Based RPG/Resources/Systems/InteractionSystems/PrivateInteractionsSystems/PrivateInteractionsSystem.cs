e1d61e03d38dbde10306aee3c2bf15d1c7f87571
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:20:20 +0100

    New PrivateInteractionsSystem gives subscribes an extension to hold PrivateInteractions, which are interactions they hold upon themselves, about other objects

--- Little Choice-Based RPG/Resources/Systems/InteractionSystems/PrivateInteractionsSystems/PrivateInteractionsSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/InteractionSystems/PrivateInteractionsSystems/PrivateInteractionsSystem.cs
@@ -9,18 +9,19 @@ using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.InteractionSystems.PrivateInteractionsSystems
 {
-    /// <summary> Implements player-specific logic.</summary>
+    /// <summary> Implements the logic for interactions only meant to be seen by the player. This isn't currently enforced in any way.</summary>
     public class PrivateInteractionsSystem : PropertyLogic
     {
         protected override void InitialiseNewSubscriber(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
             //Give the subscriber a PrivateInteractions list.
-            if (!sourceContainer.Extensions.ContainsExtension("PrivateInteractions")) //If a PrivateInteractions isnt already assigned
+            if (!sourceContainer.Extensions.Contains("PrivateInteractions")) //If a PrivateInteractions isnt already assigned
                 sourceContainer.Extensions.AddExtension(new PrivateInteractions());
         }
 
         protected override void OnObjectChanged(object sender, ObjectChangedEventArgs objectChangedData)
         {
+            //Not needed in this class yet! :) 
         }
     }
 }
