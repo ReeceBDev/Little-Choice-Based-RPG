5248645ae29d13158ccbdaf37493c2ad19764b90
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:22:47 +0100

    New DescriptorSystem which is responsible for requiring common descriptors upon propertycontainers. It also provides functionality for retriving the descriptors in a way which can be correctly iterated upon from one place.

--- Little Choice-Based RPG/Resources/Systems/InformationalSystems/Descriptor/DescriptorSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/InformationalSystems/Descriptor/DescriptorSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,54 @@ namespace Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descrip
 {
     public class DescriptorSystem : PropertyLogic
     {
+        static DescriptorSystem()
+        {
+            PropertyValidation.CreateValidProperty("Descriptor.Generic.Current", PropertyType.String); //Generic Descriptor the object is currently using
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Current", PropertyType.String); //Inspect Descriptor the object is currently using
+
+            PropertyValidation.CreateValidProperty("Descriptor.Generic.Default", PropertyType.String);
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default", PropertyType.String);
+
+
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default.IsAdditive", PropertyType.Boolean);
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default.Additive.Suffix", PropertyType.String);
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default.Additive.Infix", PropertyType.String);
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default.Additive.Prefix", PropertyType.String);
+            PropertyValidation.CreateValidProperty("Descriptor.Inspect.Default.Additive.Empty", PropertyType.String);
+        }
+
+        protected override void InitialiseNewSubscriber(PropertyContainer targetContainer, PropertyHandler sourceProperties)
+        {
+            //Freeze default descriptors.
+            sourceProperties.FreezeProperty("Descriptor.Generic.Default");
+            sourceProperties.FreezeProperty("Descriptor.Inspect.Default");
+
+            //If a descriptor is Additive, check that it has its associated additive modifiers: Prefix, Infix, Suffix and Empty.
+            foreach (var property in targetContainer.Properties.EntityProperties)
+                if (property.PropertyName.StartsWith("Descriptor.") && property.PropertyName.EndsWith(".IsAdditive") && property.PropertyValue.Equals(true))
+                {
+                    //Grab the descriptors inner identity
+                    string descriptorName =
+                        property.PropertyName.Remove(property.PropertyName.Length - ".IsAdditive".Length);
+
+                    //Check it has its associated additive modifiers
+                    if (!targetContainer.Properties.HasExistingPropertyName($"{descriptorName}.Additive.Prefix"))
+                        throw new Exception($"This descriptors parent {targetContainer} didn't contain a reference for \"{descriptorName}.Prefix\" in its properties list {targetContainer.Properties}!");
+
+                    if (!targetContainer.Properties.HasExistingPropertyName($"{descriptorName}.Additive.Infix"))
+                        throw new Exception($"This descriptors parent {targetContainer} didn't contain a reference for \"{descriptorName}.Infix\" in its properties list {targetContainer.Properties}!");
+
+                    if (!targetContainer.Properties.HasExistingPropertyName($"{descriptorName}.Additive.Suffix"))
+                        throw new Exception($"This descriptors parent {targetContainer} didn't contain a reference for \"{descriptorName}.Suffix\" in its properties list {targetContainer.Properties}!");
+
+                    if (!targetContainer.Properties.HasExistingPropertyName($"{descriptorName}.Additive.Empty"))
+                        throw new Exception($"This descriptors parent {targetContainer} didn't contain a reference for \"{descriptorName}.Suffix\" in its properties list {targetContainer.Properties}!");
+                }
+        }
+
+        protected override void OnObjectChanged(object sender, ObjectChangedEventArgs propertyChangedData)
+        {
+            //Empty at the moment for this system.
+        }
     }
 }
