d63008f907edff23235867fba930be55737fd8fe
ReeceBDev <reece.bowen99@gmail.com>
2025-04-25 23:15:19 +0100

    New InventoryPropertyValidation is used to enforce pickupable objects with relevant descriptors, as long as they fall under the new Global Maximum Value of Maximum Strength.
    As this maximum value is changed over time as the game changes, the InventoryPropertyValidation will see more objects that require the descriptors it nforces.
    
    This class came about because items should not have to subscribe to anything to get verified, and as such, this logic is ran elsewhere.

---
+++ Little Choice-Based RPG/Resources/Systems/ContainerSystems/Inventory/InventoryPropertyValidation.cs
@@ -0,0 +1,49 @@
+﻿using Little_Choice_Based_RPG.Managers.World;
+using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players;
+using Little_Choice_Based_RPG.Resources.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory
+{
+    public static class InventoryPropertyValidation
+    {
+        public static void ValidateInventoryDescriptors(GameObject target)
+        {
+            //Check weight against global maximum strength value
+            decimal currentWeight = (decimal)target.Properties.GetPropertyValue("WeightInKG");
+
+            if (currentWeight > GlobalMaximumValues.MaximumStrengthLimit)
+                return; //Inventory descriptors aren't required since it falls over the maximum strength limit.
+
+            //Ensure pickup and putdown descriptors exist
+            if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Pickup.Title"))
+                throw new Exception($"The GameObject or derivative {target}, weighed less than the maximum strength limit, but didn't have a Pickup Title descriptor!");
+
+            if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Pickup.Invoking"))
+                throw new Exception($"The GameObject or derivative {target}, weighed less than the maximum strength limit, but didn't have a Pickup Invoking descriptor!");
+
+            if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Drop.Title"))
+                throw new Exception($"The GameObject or derivative {target}, weighed less than the maximum strength limit, but didn't have a Pickup Title descriptor!");
+
+            if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Drop.Invoking"))
+                throw new Exception($"The GameObject or derivative {target}, weighed less than the maximum strength limit, but didn't have a Pickup Invoking descriptor!");
+        }
+
+        public static void ValidateOpenInventoryDescriptors(PropertyContainer target)
+        {
+            if ((target is not Room) && (target is not Player))
+            {
+                if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Open.Title"))
+                    throw new Exception($"The GameObject or derivative {target}, was an openable container, but didn't have an Open Title descriptor!");
+
+                if (!target.Properties.HasExistingPropertyName("Descriptor.InventorySystem.Interaction.Open.Invoking"))
+                    throw new Exception($"The GameObject or derivative {target}, was an openable container, but didn't have the Open Invoking descriptor!");
+            }
+        }
+    }
+}
