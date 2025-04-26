dd79867ad56cafc2d32b800f1b8ebd1103ac8ed5
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:32:17 +0100

    New RepairProcessor holds all the repair logic now, such as InvokeRepair

---
+++ Little Choice-Based RPG/Resources/Systems/DamageSystems/Repair/RepairProcessor.cs
@@ -0,0 +1,59 @@
+﻿using Little_Choice_Based_RPG.Managers.Player_Manager;
+using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descriptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Repair
+{
+    public static class RepairProcessor
+    {
+        /// <summary> Sets Damage.Broken to false, from the BreakSystem. </summary>
+        public static void InvokeRepair(PlayerController mutexHolder, PropertyContainer target) => InvokeRepair(mutexHolder, target, null);
+
+        /// <summary> Sets Damage.Broken to false, from the BreakSystem. </summary>
+        public static void InvokeRepair(PlayerController mutexHolder, PropertyContainer target, GameObject? repairTool = null)
+        {
+            //Guard clauses for the values in use.
+            if (!target.Properties.HasPropertyAndValue("Damage.Broken", true))
+                throw new Exception("This object is not broken! Tried to repair an object where there is no EntityProperty of Damage.Broken = true.");
+
+            //Check if a repair tool is required.
+            if (!target.Properties.HasPropertyAndValue("Repairable.RequiresTool", true))
+            {
+                //When no repair tool is required, initate repair.
+                Repair(target);
+                return;
+            }
+
+            //When a repair tool is required:
+            if (repairTool == null)
+                return; // Error, can't repair without a tool!
+
+            //If repair tool doesn't match
+            if (!repairTool.Properties.HasPropertyAndValue("Repairable.ToolType", target.Properties.GetPropertyValue("Repairable.RequiredToolType")))
+                return; //Error, repairtooltype doesn't match the requiredrepairtool on this object
+
+            //Repair if repair tool is correct :)
+            Repair(target);
+        }
+
+        private static void Repair(PropertyContainer target)
+        {
+            //Main repairing logic
+            target.Properties.UpsertProperty("Damage.Broken", false); //Property found in DamageLogic
+
+            SetRepairDescriptors(target);
+        }
+
+        public static void SetRepairDescriptors(PropertyContainer target)
+        {
+            //Set the generic and inspect descriptors back to default.
+            target.Properties.UpsertProperty("Descriptor.Generic.Current", DescriptorProcessor.GetDescriptor(target, "Generic.Default"));
+            target.Properties.UpsertProperty("Descriptor.Inspect.Current", DescriptorProcessor.GetDescriptor(target, "Generic.Default"));
+        }
+    }
+}
