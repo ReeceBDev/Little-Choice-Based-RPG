361b5b4ecab6296ca9e30403a79fe3ec562c76c8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:32:56 +0100

    Major changes to BreakSystem to bring it inline with current implementations. Split off the logic into its own files/

--- Little Choice-Based RPG/Resources/Systems/DamageSystems/Break/BreakSystem.cs
+++ Little Choice-Based RPG/Resources/Systems/DamageSystems/Break/BreakSystem.cs
@@ -1,19 +1,23 @@
-﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
+﻿using Little_Choice_Based_RPG.Managers.Player_Manager;
+using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs;
 using Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Repair;
+using Little_Choice_Based_RPG.Resources.Systems.InteractionSystems.PublicInteractionsSystems;
 using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types.EntityProperties;
-using Little_Choice_Based_RPG.Types.Interactions.Delegates;
+using Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
-using static Little_Choice_Based_RPG.Types.Interactions.Delegates.InteractionUsingNothing;
+using static Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates.InteractionUsingNothing;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Break
 {
+    /// <summary> Allows objects to break. </summary>
     public class BreakSystem : PropertyLogic
     {
         static BreakSystem()
@@ -28,26 +32,25 @@ namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Break
             PropertyValidation.CreateValidProperty("Descriptor.Inspect.Broken", PropertyType.String); //A closer look
         }
 
-        /// <summary> Allows objects to break. Requires DamageLogic. </summary>
-        public BreakSystem()
+        protected override void OnObjectChanged(object sender, ObjectChangedEventArgs propertyChangedData)
         {
-            DamageLogic damageCommonInstantiation = DamageLogic.Instance;
-        }
-
-        protected override void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedData)
-        {
-            PropertyContainer sourceContainer = propertyChangedData.sourceContainer;
-            PropertyHandler sourceProperties = sourceContainer.Properties;
-            EntityProperty newProperty = propertyChangedData.newProperty;
+            PropertyContainer modifiedContainer = propertyChangedData.Source;
+            PropertyHandler modifiedProperties = modifiedContainer.Properties;
 
             // When to do with Damage.Broken
-            if (newProperty.PropertyName == "Damage.Broken")
+            if (propertyChangedData.Property == "Damage.Broken")
             {
                 // When something breakable is repaired
-                if (newProperty.PropertyName == "Damage.Broken" && newProperty.PropertyValue.Equals(false))
+                if (propertyChangedData.Property == "Damage.Broken" && propertyChangedData.Change.Equals(false))
                 {
-                    if (sourceProperties.HasProperty("Breakable.ByChoice", true))
-                        InitialiseBreakChoices(sourceContainer, sourceProperties);
+                    if (modifiedProperties.HasPropertyAndValue("Breakable.ByChoice", true))
+                        PublicInteractionsLogic.AddPublicInteraction(modifiedContainer, BreakDelegation.GetBreakUsingNothing(modifiedContainer, modifiedProperties));
+                }
+
+                // When something breakable is broken, remove its choice
+                if (propertyChangedData.Property == "Damage.Broken" && propertyChangedData.Change.Equals(true))
+                {
+                    PublicInteractionsLogic.TryRemovePublicInteraction(modifiedContainer, BreakDelegation.GetBreakUsingNothing(modifiedContainer, modifiedProperties));
                 }
             }
         }
@@ -55,59 +58,15 @@ namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Break
         protected override void InitialiseNewSubscriber(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
         {
             // If the object is not broken
-            if (sourceProperties.HasProperty("Damage.Broken", false))
+            if (!sourceProperties.HasPropertyAndValue("Damage.Broken", true))
             {
-                if (sourceProperties.HasProperty("Breakable.ByChoice", true))
-                    InitialiseBreakChoices(sourceContainer, sourceProperties);
+                if (sourceProperties.HasPropertyAndValue("Breakable.ByChoice", true))
+                    PublicInteractionsLogic.AddPublicInteraction(sourceContainer, BreakDelegation.GetBreakUsingNothing(sourceContainer, sourceProperties));
             }
-        }
 
-
-        private void InitialiseBreakChoices(PropertyContainer sourceContainer, PropertyHandler sourceProperties)
-        {
-            //Guard clauses for interaction descriptors
-            if (!sourceProperties.HasExistingPropertyName("Descriptor.Breakable.Interaction.Title"))
-                throw new Exception($"{sourceContainer} of ID {sourceProperties.GetPropertyValue("ID")} has no property of Descriptor.Breakable.Interaction.Title!");
-
-            if (!sourceProperties.HasExistingPropertyName("Descriptor.Breakable.Interaction.Invoking"))
-                throw new Exception($"{sourceContainer} of ID {sourceProperties.GetPropertyValue("ID")} has no property of Descriptor.Breakable.Interaction.Invoking!");
-
-
-            //Initialise the Interaction for Break()
-            InteractionUsingNothingDelegate breakUsingNothingDelegate = Break;
-
-            InteractionUsingNothing breakUsingNothing = new InteractionUsingNothing(
-                breakUsingNothingDelegate,
-                sourceContainer,
-                (string)sourceProperties.GetPropertyValue("Descriptor.Breakable.Interaction.Title"),
-                (string)sourceProperties.GetPropertyValue("Descriptor.Breakable.Interaction.Invoking")
-            );
-
-            GiveInteraction(sourceContainer, breakUsingNothing);
-        }
-
-
-        /// <summary> Sets Damage.Broken to true. </summary>
-        public void Break(PlayerController mutexHolder, PropertyContainer sourceContainer)
-        {
-            //Guard clauses for the values in use.
-            if (sourceContainer.Properties.HasProperty("Damage.Broken", true))
-                throw new Exception("This object is already broken! Tried to break an object where there is already an EntityProperty of Damage.Broken = true.");
-
-            if (!sourceContainer.Properties.HasExistingPropertyName("Descriptor.Generic.Broken"))
-                throw new Exception("This object has no broken description! Tried to break an object where there is no EntityProperty of Descriptor.Generic.Broken.");
-
-            //Main breaking logic.
-            sourceContainer.Properties.UpsertProperty("Damage.Broken", true); //Property found in DamageLogic
-
-            //Set generic descriptor.
-            sourceContainer.Properties.ReplaceProperty("Descriptor.Generic.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
-
-            //Set inspect descriptor or default to generic.
-            if (!sourceContainer.Properties.HasExistingPropertyName("Descriptor.Inspect.Broken"))
-                sourceContainer.Properties.ReplaceProperty("Descriptor.Inspect.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Inspect.Broken"));
-            else
-                sourceContainer.Properties.ReplaceProperty("Descriptor.Inspect.Current", sourceContainer.Properties.GetPropertyValue("Descriptor.Generic.Broken"));
+            //If the object is already broken
+            if (sourceProperties.HasPropertyAndValue("Damage.Broken", true))
+                BreakLogic.SetBrokenDescriptors(sourceContainer); //Ensure its descriptors are correct.
         }
     }
 }
