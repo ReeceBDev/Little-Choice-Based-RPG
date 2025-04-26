b0232c5493a61d66fb85588036c6660e0ee14256
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:34:24 +0100

    Gave players a weight and custom descriptor to describe themselves

--- Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
+++ Little Choice-Based RPG/Resources/Entities/Physical/Living/Players/Player.cs
@@ -11,7 +11,6 @@ using Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour;
 using Little_Choice_Based_RPG.Resources.Rooms;
 using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Weightbearing_Derivatives;
-using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Descriptive_Derivatives;
 using Little_Choice_Based_RPG.Types.EntityProperties;
 
 namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
@@ -20,6 +19,7 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
     {
         private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
         {
+            { "Descriptor.Player.Custom", PropertyType.String }
         };
 
         private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
@@ -39,7 +39,11 @@ namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players
             {"Player.CanSee", true },
 
             //Stats
-            {"Weightbearing.StrengthInKG",  32.0m}
+            {"Weightbearing.StrengthInKG",  32.0m},
+            {"WeightInKG", 78.4m},
+
+            //Custom descriptor
+            {"Descriptor.Player.Custom", "A man falls from a tear in reality, thrown forwards without footing and clipped at by cold, sharp airs and a stomach-pit of sudden peril. Darkly dressed without a sliver of skin, behind his jacket a nictation of the lapel reveals a dark silver-grey gunmetal contraption branded with one of the traditional marks of the Potsun Burran, meaning scavenger. He is you.\n\nOut of the void of glass, ensnared in whispers of iridescent whisps of near translucent smoke, white whispers of reality fluttering away having been dragged out of the mirror-esque and clinging with futility to the edges of your self. You stagger clear of the rupture and catch yourself, your boots hitting the ground with a thud, grains of sand thrown from the edges of your boots."}
         };
 
         static Player()
