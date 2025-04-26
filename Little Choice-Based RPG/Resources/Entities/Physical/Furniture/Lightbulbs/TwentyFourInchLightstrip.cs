91d971fb2590a5086c2b43e819b105520211e2f8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:34:35 +0100

    Gave TwentyFourInchLightstrip a weight of 5.8kg

---
+++ Little Choice-Based RPG/Resources/Entities/Physical/Furniture/Lightbulbs/TwentyFourInchLightstrip.cs
@@ -0,0 +1,50 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual.Interactions;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.Physical.Furniture.Lightbulbs
+{
+    internal class TwentyFourInchLightstrip : Lightbulb
+    {
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            { "WeightInKG", 5.8m },
+        };
+
+        static TwentyFourInchLightstrip()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        public TwentyFourInchLightstrip(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+    }
+}
