37af70004d4a3170f476fdeeef726571e032eb3b
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:30:04 +0100

    Moved existing classes to a new namespace under ImmaterialEntities.

---
+++ Little Choice-Based RPG/Resources/Entities/ImmaterialEntities/Immaterial.cs
@@ -0,0 +1,59 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.ImmaterialEntities
+{
+    public class Immaterial : PropertyContainer
+    {
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+            {"IsImmaterial", PropertyType.Boolean},
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            { "IsImmaterial", true },
+            { "Name", "Unknown Immaterial" }
+        };
+
+        static Immaterial()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected Immaterial(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            //Update the default name to contain the type
+            if (this.Properties.HasPropertyAndValue("Name", "Unknown Immaterial"))
+                this.Properties.UpsertProperty("Name", $"Unknown Immaterial of type \"{GetType().Name.ToString()}\"");
+
+
+            //Freeze IsImmaterial property.
+            Properties.FreezeProperty("IsImmaterial");
+
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+    }
+}
---
+++ Little Choice-Based RPG/Resources/Entities/ImmaterialEntities/Transition/RoomDirection.cs
@@ -0,0 +1,75 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Little_Choice_Based_RPG.Resources.Entities.ImmaterialEntities.Transition
+{
+    public class RoomDirection : Immaterial
+    {
+
+        private readonly static Dictionary<string, PropertyType> requiredProperties = new Dictionary<string, PropertyType>()
+        {
+            {"Direction.IsVisible", PropertyType.Boolean},
+            {"Direction.DestinationRoomID", PropertyType.UInt32 },
+
+            {"Descriptor.Direction.Travelling", PropertyType.String}
+        };
+
+        private readonly static Dictionary<string, PropertyType> optionalProperties = new Dictionary<string, PropertyType>()
+        {
+            {"Direction.AssociatedObjectID", PropertyType.UInt32},
+        };
+
+        private readonly static Dictionary<string, object> defaultProperties = new Dictionary<string, object>()
+        {
+            {"Direction.IsVisible", true},
+            {"Descriptor.Direction.Travelling", "Feet scraping flecks of shrubbery drowned in dust scrambling from cracked sandstone, you forge your way further across the desolation."}
+        };
+
+        static RoomDirection()
+        {
+            //Define new required and optional ValidProperties for this class
+            DeclareNewPropertyTypes(requiredProperties);
+            DeclareNewPropertyTypes(optionalProperties);
+        }
+
+        private protected RoomDirection(Dictionary<string, object>? derivedProperties = null)
+            : base(SetLocalProperties(derivedProperties ??= new Dictionary<string, object>()))
+        {
+            // When the direction is associated with an object, make it invisible by default.
+            // This forces the implementing object to reveal the direction when accessible, i.e. a Door being open.
+            if (Properties.HasExistingPropertyName("Direction.AssociatedObjectID"))
+                Properties.UpsertProperty("Direction.IsVisible", false);
+
+            //Validate required properties have been set on entityProperties
+            ValidateRequiredProperties(requiredProperties);
+        }
+
+        private static Dictionary<string, object> SetLocalProperties(Dictionary<string, object> derivedProperties)
+        {
+            //Apply default properties for this class to the current list of derivedProperties
+            ApplyDefaultProperties(derivedProperties, defaultProperties);
+
+            return derivedProperties; //Return is required to give (base) the derived list.
+        }
+
+
+        public uint GetDestinationID(uint? associatedObjectID = null) // associatedObjectID should be the calling object. Throws an exception if the associatedObjectID does not match the one stored on this direction in entityProperties.
+        {
+            //If no associated object is required, return 
+            if (!Properties.HasExistingPropertyName("Direction.AssociatedObjectID"))
+                return (uint) Properties.GetPropertyValue("Direction.DestinationRoomID");
+
+            //Check that the associated object matches the one stored in entityProperties
+            if (associatedObjectID != (uint)Properties.GetPropertyValue("Direction.AssociatedObjectID"))
+                throw new Exception("The associated object ID did not match the one stored in entityProperties!");
+
+            return (uint)Properties.GetPropertyValue("Direction.DestinationRoomID");
+        }
+    }
+}
