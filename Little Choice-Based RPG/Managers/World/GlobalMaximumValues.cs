9d236f3ffe57a496ad22b0e2fac15f62448d7272
ReeceBDev <reece.bowen99@gmail.com>
2025-04-26 22:24:28 +0100

    New GlobalMaximumValues which holds the new MaximumStrengthLimit. Currently this is set to 35 kilogrammes, as that is just over my players' weight capacity.

---
+++ Little Choice-Based RPG/Managers/World/GlobalMaximumValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Managers.World
+{
+    public static class GlobalMaximumValues
+    {
+        public const decimal MaximumStrengthLimit = 35.0m;
+    }
+}
