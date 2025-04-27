2bf6781af7f331c1c7b2ec82a93d5305a3a18963
ReeceBDev <reece.bowen99@gmail.com>
2025-04-27 23:46:01 +0100

    InteractionController has been revamped. - Rebuilt GetInteractions for accessing public interactions - Added new GetPrivateInteractions for accessing the private interactions stored on a player. - Removed old, redundnant code. - Namespace updates.

--- Little Choice-Based RPG/Managers/Player Manager/Frontend/InteractionController.cs
+++ Little Choice-Based RPG/Managers/Player Manager/Frontend/InteractionController.cs
@@ -1,11 +1,16 @@
 ﻿using Little_Choice_Based_RPG.Managers.Player_Manager.Frontend.UserInterface;
 using Little_Choice_Based_RPG.Managers.World;
+using Little_Choice_Based_RPG.Resources;
 using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
 using Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players;
 using Little_Choice_Based_RPG.Resources.Rooms;
+using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory;
+using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory.InventoryExtensions;
+using Little_Choice_Based_RPG.Resources.Systems.InteractionSystems.PrivateInteractionsSystems.PrivateInteractionsExtensions;
 using Little_Choice_Based_RPG.Types;
 using Little_Choice_Based_RPG.Types.EntityProperties;
-using Little_Choice_Based_RPG.Types.Interactions.InteractDelegate;
+using Little_Choice_Based_RPG.Types.Interactions;
+using Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +22,15 @@ namespace Little_Choice_Based_RPG.Managers.Player_Manager.Frontend
 {
     public static class InteractionController
     {
-        /// <summary> Returns a list of interactions based on the required context. </summary>
-        public static List<IInvokableInteraction>? GetInteractions(InteractionRole ofContext, Room currentRoom) //May return nothing, so I've declared the return type as nullable.
+        /// <summary> Returns a list of interactions based on the required context. Objects may be filtered. </summary>
+        public static List<IInvokableInteraction>? GetInteractions(Room currentRoom, InteractionRole ofContext, EntityProperty setFilter)
+            => GetInteractions(currentRoom, ofContext, setFilter);
+
+        /// <summary> Returns a list of interactions by interaction context. Objects may be filtered. </summary>
+        public static List<IInvokableInteraction> GetInteractions(Room currentRoom, InteractionRole ofContext, List<EntityProperty>? setFilters = null)
         {
             var relevantInteractions = new List<IInvokableInteraction>();
-            var targetObjects = new List<GameObject>(currentRoom.GetRoomObjects());
+            List<GameObject> targetObjects = InventoryProcessor.GetInventoryEntities(currentRoom, setFilters);
 
             foreach (GameObject target in targetObjects)
             {
@@ -36,20 +45,56 @@ namespace Little_Choice_Based_RPG.Managers.Player_Manager.Frontend
         }
 
         /// <summary> Returns a list of all possible interactions on a particular GameObject. </summary>
-        public static List<IInvokableInteraction>? GetInteractions(GameObject ofGameObject, Room currentRoom) //May return nothing, so I've declared the return type as nullable.
+        public static List<IInvokableInteraction> GetInteractions(GameObject targetObject)
         {
-            var relevantInteractions = new List<IInvokableInteraction>();
-            var targetObjects = new List<GameObject>(currentRoom.GetRoomObjects());
+            List<IInvokableInteraction> relevantInteractions = new();
 
-            if (!currentRoom.RoomContainsObject(ofGameObject))
-                throw new ArgumentException($"The requested GameObject does not exist in this room! There is no local GameObject matching {ofGameObject} in room {currentRoom}.");
-
-            foreach (IInvokableInteraction newInteraction in ofGameObject.Interactions)
-            {
+            foreach (IInvokableInteraction newInteraction in targetObject.Interactions)
                 relevantInteractions.Add(newInteraction);
-            }
 
             return relevantInteractions;
+        }
+
+
+        /// <summary> Returns a list of private interactions on a player by their interaction context. </summary>
+        public static List<IInvokableInteraction> GetPrivateInteractions(Player currentPlayer, InteractionRole ofContext)
+        {
+            if (!currentPlayer.Extensions.Contains("PrivateInteractions"))
+                throw new ArgumentException($"The current player, {currentPlayer} does not contain an extension named \"PrivateInteractions\" and therefore can't hold the private interactions! Extensions list: {currentPlayer.Extensions}.");
+
+
+            var retrievedInteractions = new List<IInvokableInteraction>();
+            PrivateInteractions playerInteractions = (PrivateInteractions)currentPlayer.Extensions.Get("PrivateInteractions");
+            
+            //Grab each interaction which matches the interaction context
+            foreach (KeyValuePair<PropertyContainer, IInvokableInteraction> interactionKeyValuePair in playerInteractions.PrivateInteractionsList)
+            {
+                if (interactionKeyValuePair.Value.InteractionContext == ofContext)
+                    retrievedInteractions.Add(interactionKeyValuePair.Value);
+            }
+
+            return retrievedInteractions;
+        }
+
+        /// <summary> Returns a list of all private interactions on a player, for a particular GameObject. </summary>
+        public static List<IInvokableInteraction> GetPrivateInteractions(Player currentPlayer, GameObject targetObject)
+        {
+            if (!currentPlayer.Extensions.Contains("PrivateInteractions"))
+                throw new ArgumentException($"The current player, {currentPlayer} does not contain an extension named \"PrivateInteractions\" and therefore can't hold the private interactions! Extensions list: {currentPlayer.Extensions}.");
+
+
+            var retrievedInteractions = new List<IInvokableInteraction>();
+            PrivateInteractions playerInteractions = (PrivateInteractions)currentPlayer.Extensions.Get("PrivateInteractions");
+
+
+            //Grab each interaction which matches the targetObject
+            foreach (KeyValuePair<PropertyContainer, IInvokableInteraction> interactionKeyValuePair in playerInteractions.PrivateInteractionsList)
+            {
+                if (interactionKeyValuePair.Key == targetObject)
+                    retrievedInteractions.Add(interactionKeyValuePair.Value);
+            }
+
+            return retrievedInteractions;
         }
     }
 }
