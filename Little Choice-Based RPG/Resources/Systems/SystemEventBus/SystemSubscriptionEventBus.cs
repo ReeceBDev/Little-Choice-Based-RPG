db42f0e947607be6754077fa92c68269220c04f7
ReeceBDev <reece.bowen99@gmail.com>
2025-04-27 23:44:37 +0100

    Added new Unsubription to the SystemSubscriptionEventBus for enabling resubscriptions, which will change the event handling order.

--- Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionEventBus.cs
+++ Little Choice-Based RPG/Resources/Systems/SystemEventBus/SystemSubscriptionEventBus.cs
@@ -1,8 +1,10 @@
 ﻿using Little_Choice_Based_RPG.Resources.Systems.DamageSystems;
+using Little_Choice_Based_RPG.Types.EntityProperties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Little_Choice_Based_RPG.Resources.Systems.SystemEventBus
@@ -10,8 +12,12 @@ namespace Little_Choice_Based_RPG.Resources.Systems.SystemEventBus
     public static class SystemSubscriptionEventBus
     {
         public static event EventHandler<SystemSubscriptionRequestEventArgs> SystemSubcriptionRequest;
+        public static event EventHandler<SystemSubscriptionRequestEventArgs> SystemUnsubscriptionRequest;
 
         public static void Subscribe(PropertyContainer targetObject, string systemReferenceName) =>
             SystemSubcriptionRequest?.Invoke(targetObject, new SystemSubscriptionRequestEventArgs(targetObject, systemReferenceName));
+
+        public static void Unsubscribe(PropertyContainer targetObject, string systemReferenceName) =>
+            SystemUnsubscriptionRequest?.Invoke(targetObject, new SystemSubscriptionRequestEventArgs(targetObject, systemReferenceName));
     }
 }
