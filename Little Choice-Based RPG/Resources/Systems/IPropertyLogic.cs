278553f610b03f88e4df42aa16828d8607a25bc6
ReeceBDev <reece.bowen99@gmail.com>
2025-04-27 23:47:11 +0100

    New IPropertyLogic adds an interface for PropertyLogic, containing OnObjectChanged and InitialiseNewSubscriber.

---
+++ Little Choice-Based RPG/Resources/Systems/IPropertyLogic.cs
@@ -0,0 +1,19 @@
+﻿using Little_Choice_Based_RPG.Resources.PropertyContainerEventArgs;
+using Little_Choice_Based_RPG.Types.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Resources.Systems
+{
+    public interface IPropertyLogic
+    {
+        /// <summary> Provide logic for co-ordinating property changes with their relevant methods. </summary>
+        public static abstract void OnObjectChanged(object sender, ObjectChangedEventArgs propertyChangedData);
+
+        /// <summary> Apply starting implementation to a subscription. This may include handling the initial interactions based on its initial property state. </summary>
+        public static abstract void InitialiseNewSubscriber(PropertyHandler sourceProperties);
+    }
+}
