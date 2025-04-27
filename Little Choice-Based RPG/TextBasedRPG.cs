ee3f3ebfb6e433e2b524f0f8c15da947f8076cf8
ReeceBDev <reece.bowen99@gmail.com>
2025-04-27 23:57:08 +0100

    Updated main to hold the new Systems and spawn the player as an individual with all player spawning logic handled by the PlayerController now.

--- Little Choice-Based RPG/TextBasedRPG.cs
+++ Little Choice-Based RPG/TextBasedRPG.cs
@@ -17,16 +17,17 @@ using Little_Choice_Based_RPG.Resources.Entities.Physical.Equippables.Armour;
 using Little_Choice_Based_RPG.Resources.Entities.Physical.Furniture;
 using Little_Choice_Based_RPG.Resources.Entities.Physical.Living.Players;
 using Little_Choice_Based_RPG.Resources.Rooms;
-using Little_Choice_Based_RPG.Resources.Rooms.Premade.Unique.Test;
 using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems;
 using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Attach;
 using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Gear;
 using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory;
+using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Weightbearing;
+using Little_Choice_Based_RPG.Resources.Systems.DamageSystems;
 using Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Break;
 using Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Flammability;
 using Little_Choice_Based_RPG.Resources.Systems.DamageSystems.Repair;
-using Little_Choice_Based_RPG.Resources.Systems.DescriptorSystems.Descriptor;
 using Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descriptor;
+using Little_Choice_Based_RPG.Resources.Systems.InteractionSystems.PrivateInteractionsSystems;
 using Little_Choice_Based_RPG.Resources.Systems.PlayerSystems;
 using Little_Choice_Based_RPG.Resources.Systems.SystemEventBus;
 using Little_Choice_Based_RPG.Types;
@@ -36,7 +37,8 @@ internal class TextBasedRPG
 {
     private static void Main(string[] args)
     {
-        //Initialise the current systems:
+        //Initialise the current systems:        
+        RuntimeHelpers.RunClassConstructor(typeof(GlobalDamageProperties).TypeHandle);
         new FlammabilitySystem();
         new RepairSystem();
         new BreakSystem();
@@ -45,6 +47,8 @@ internal class TextBasedRPG
         new InventorySystem();
         new AttachSystem();
         new DescriptorSystem();
+        new WeightbearingSystem();
+        new PrivateInteractionsSystem();
 
         //Generate the environment
         GameEnvironment currentEnvironment = new GameEnvironment();
