9ded58aea2516c48250da8a649c3badf65cf4600
ReeceBDev <reece.bowen99@gmail.com>
2025-04-28 03:47:01 +0100

    Added Directions for automated map generation and the upcoming navigation system.

---
+++ Little Choice-Based RPG/Types/Navigation/Directions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.Navigation
+{
+    public enum Directions
+    {
+        North,
+        Northeast,
+        East,
+        Southeast,
+        South,
+        Southwest,
+        West,
+        Northwest,
+        Up,
+        Down
+    }
+}
+
