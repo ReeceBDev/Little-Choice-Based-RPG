01b13e8fe74d639cb2b2ef99124b0ff1a791f0fa
ReeceBDev <reece.bowen99@gmail.com>
2025-04-29 22:22:17 +0100

    Added new Coordinates type which represents points in a 3D space along an X-Axis, Y-Axis and Z-Axis.

---
+++ Little Choice-Based RPG/Types/Navigation/Coordinates.cs
@@ -0,0 +1,12 @@
+﻿using Little_Choice_Based_RPG.Resources.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Choice_Based_RPG.Types.Navigation
+{
+    /// <summary> Coordinates representing a 3D space, along the X-Axis, Y-Axis and Z-Axis, respectively. </summary>
+    public readonly record struct Coordinates(int X, int Y, int Z);
+}
