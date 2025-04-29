f393c863719c49cf7780cd9994924e3424b1ad9f
ReeceBDev <reece.bowen99@gmail.com>
2025-04-29 22:28:58 +0100

    Added new DirectionDelegation to generate the travel interaction which will be gievn to players for the new DirectionSystem

---
+++ Little Choice-Based RPG/Resources/Systems/RoomSystems/DirectionDelegation.cs
@@ -0,0 +1,37 @@
+﻿using Little_Choice_Based_RPG.Resources.Entities.Conceptual;
+using Little_Choice_Based_RPG.Resources.Rooms;
+using Little_Choice_Based_RPG.Resources.Systems.ContainerSystems.Inventory;
+using Little_Choice_Based_RPG.Resources.Systems.InformationalSystems.Descriptor;
+using Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates.TripleParameterDelegates;
+using Little_Choice_Based_RPG.Types.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates.TripleParameterDelegates.InteractionUsingCurrentPlayerAndCurrentRoomAndPreassignedRoom;
+using static Little_Choice_Based_RPG.Types.Interactions.InteractionDelegates.TripleParameterDelegates.InteractionUsingTwoGameObjectsAndCurrentPlayer;
+
+namespace Little_Choice_Based_RPG.Resources.Systems.RoomSystems
+{
+    public static class DirectionDelegation
+    {
+        public static InteractionUsingCurrentPlayerAndCurrentRoomAndPreassignedRoom NewChangeRoom(PropertyContainer destination, CardinalDirection? direction)
+        {
+            string directionName = direction.Value.ToString() ?? "Travel";
+
+            InteractionUsingCurrentPlayerAndCurrentRoomAndPreassignedRoomDelegate changeRoomDelegate = InventoryProcessor.MoveBetweenInventories;
+
+            //(Delegate setDelegate, string setInteractTitle, string setInteractDescriptor, string requestDescription, GameObject preassignedParameter2, InteractionRole setInteractRole = InteractionRole.Explore, List<EntityProperty>? gameObjectRequestFilter = null)
+            var changeRoom = new InteractionUsingCurrentPlayerAndCurrentRoomAndPreassignedRoom
+            (
+                changeRoomDelegate,
+                $"{directionName} - {DescriptorProcessor.GetDescriptor(destination, "DirectionSystem.Interaction.Travel.Title")}",
+                DescriptorProcessor.GetDescriptor(destination, "DirectionSystem.Interaction.Travel.Description"),
+                (Room) destination
+            );
+
+            return changeRoom;
+        }
+    }
+}
