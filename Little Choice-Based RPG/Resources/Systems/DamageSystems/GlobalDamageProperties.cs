0cfac637ed8269c93330dfa65c070045d81ea525
ReeceBDev <reece.bowen99@gmail.com>
2025-04-30 23:52:44 +0100

    Added new GlobalDamageProperties entry Damage.Destroyed, which represents the total destruction of a given Entity.

--- Little Choice-Based RPG/Resources/Systems/DamageSystems/GlobalDamageProperties.cs
+++ Little Choice-Based RPG/Resources/Systems/DamageSystems/GlobalDamageProperties.cs
@@ -12,6 +12,7 @@ namespace Little_Choice_Based_RPG.Resources.Systems.DamageSystems
         static GlobalDamageProperties()
         {
             PropertyValidation.CreateValidProperty("Damage.Broken", PropertyType.Boolean); //Indicates when an object becomes broken. Used by BreakSystem and RepairSystem.
+            PropertyValidation.CreateValidProperty("Damage.Destroyed", PropertyType.Boolean); //Indicates whether an object is completely destroyed. Upon being destroyed, it should cease all functionality irrepairably, and possibly be deleted, and turned into a wastage object in its place, such as ash!
         }
     }
 }
